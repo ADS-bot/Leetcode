7db7b5f89f9b474f1a24bc6bc3e023b5771e726d
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-13 12:14:53 +0530

    Time: 262 ms (44.44%), Space: 16.4 MB (47.98%) - LeetHub

--- 0909-stone-game/0909-stone-game.py
+++ 0909-stone-game/0909-stone-game.py
@@ -1,16 +1,11 @@
 class Solution:
   def stoneGame(self, piles: List[int]) -> bool:
     n = len(piles)
-    # dp[i][j] := max stones you can get more than your opponent in piles[i..j]
-    dp = [[0] * n for _ in range(n)]
-
-    for i, pile in enumerate(piles):
-      dp[i][i] = pile
+    dp = piles.copy()
 
     for d in range(1, n):
-      for i in range(n - d):
-        j = i + d
-        dp[i][j] = max(piles[i] - dp[i + 1][j],
-                       piles[j] - dp[i][j - 1])
+      for j in range(n - 1, d - 1, -1):
+        i = j - d
+        dp[j] = max(piles[i] - dp[j], piles[j] - dp[j - 1])
 
-    return dp[0][n - 1] > 0
+    return dp[n - 1] > 0
