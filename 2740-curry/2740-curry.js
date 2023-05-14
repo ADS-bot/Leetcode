460a9ed0ed2179b3a8212ef9976d598bed827c38
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-14 12:00:59 +0530

    Time: 65 ms (74.76%), Space: 48.8 MB (27.46%) - LeetHub

---
+++ 2740-curry/2740-curry.js
@@ -0,0 +1,11 @@
+var curry = function(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    } else {
+      return function(...nextArgs) {
+        return curried.apply(this, args.concat(nextArgs));
+      }
+    }
+  };
+};
