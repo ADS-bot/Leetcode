688d9fc710da3a23c1f9262ea74eed927df6d505
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-17 11:13:57 +0530

    Time: 3 ms (78%), Space: 42.8 MB (16%) - LeetHub

--- 0939-valid-permutations-for-di-sequence/0939-valid-permutations-for-di-sequence.java
+++ 0939-valid-permutations-for-di-sequence/0939-valid-permutations-for-di-sequence.java
@@ -2,32 +2,31 @@ class Solution {
   public int numPermsDISequence(String s) {
     final int kMod = 1_000_000_007;
     final int n = s.length();
-    int[] dp = new int[n + 1];
+    // dp[i][j] := # of valid permutations w/ i + 1 digits, where s[i] is j-th
+    // Digit of remaining digits
+    int[][] dp = new int[n + 1][n + 1];
 
     // When there's only one digit, the # of perms is 1
     for (int j = 0; j <= n; ++j)
-      dp[j] = 1;
+      dp[0][j] = 1;
 
-    for (int i = 1; i <= n; ++i) {
-      int[] newDp = new int[n + 1];
+    for (int i = 1; i <= n; ++i)
       if (s.charAt(i - 1) == 'I') { // s[i - 1] == 'I'
         // Calculate postfix sum to prevent duplicate calculation
         int postfixsum = 0;
         for (int j = n - i; j >= 0; --j) {
-          postfixsum = (postfixsum + dp[j + 1]) % kMod;
-          newDp[j] = postfixsum;
+          postfixsum = (postfixsum + dp[i - 1][j + 1]) % kMod;
+          dp[i][j] = postfixsum;
         }
       } else { // s[i - 1] == 'D'
         // Calculate prefix sum to prevent duplicate calculation
         int prefix = 0;
         for (int j = 0; j <= n - i; ++j) {
-          prefix = (prefix + dp[j]) % kMod;
-          newDp[j] = prefix;
+          prefix = (prefix + dp[i - 1][j]) % kMod;
+          dp[i][j] = prefix;
         }
       }
-      dp = newDp;
-    }
 
-    return dp[0];
+    return dp[n][0];
   }
 }
