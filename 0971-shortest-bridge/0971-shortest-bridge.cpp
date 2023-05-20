09c7b6cde5b38fa7239f9e8bb9eac3f2f01de71f
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-20 10:29:59 +0530

    Time: 60 ms (45.59%), Space: 20.3 MB (65.61%) - LeetHub

--- 0971-shortest-bridge/0971-shortest-bridge.cpp
+++ 0971-shortest-bridge/0971-shortest-bridge.cpp
@@ -1,49 +1,61 @@
 class Solution {
  public:
   int shortestBridge(vector<vector<int>>& grid) {
-    markGridTwo(grid);
+    const int n = grid.size();
+    const vector<int> dirs{0, 1, 0, -1, 0};
+    int ans = 0;
+    queue<pair<int, int>> q;
 
-    for (int color = 2;; ++color)
-      for (int i = 0; i < grid.size(); ++i)
-        for (int j = 0; j < grid[0].size(); ++j)
-          if (grid[i][j] == color)
-            if (expand(grid, i + 1, j, color) ||  //
-                expand(grid, i - 1, j, color) ||  //
-                expand(grid, i, j + 1, color) ||  //
-                expand(grid, i, j - 1, color))
-              return color - 2;
+    markGridTwo(grid, q);
+
+    // Expand by BFS
+    while (!q.empty()) {
+      for (int sz = q.size(); sz > 0; --sz) {
+        const auto [i, j] = q.front();
+        q.pop();
+        for (int k = 0; k < 4; ++k) {
+          const int x = i + dirs[k];
+          const int y = j + dirs[k + 1];
+          if (x < 0 || x == n || y < 0 || y == n)
+            continue;
+          if (grid[x][y] == 2)
+            continue;
+          if (grid[x][y] == 1)
+            return ans;
+          grid[x][y] = 2;
+          q.emplace(x, y);
+        }
+      }
+      ++ans;
+    }
+
+    throw;
   }
 
  private:
   // Mark one group to 2s by DFS
-  void markGridTwo(vector<vector<int>>& grid) {
+  void markGridTwo(vector<vector<int>>& grid, queue<pair<int, int>>& q) {
     for (int i = 0; i < grid.size(); ++i)
       for (int j = 0; j < grid[0].size(); ++j)
         if (grid[i][j] == 1) {
-          markGridTwo(grid, i, j);
+          markGridTwo(grid, i, j, q);
           return;
         }
   }
 
-  void markGridTwo(vector<vector<int>>& grid, int i, int j) {
+  // Mark one group to 2s by DFS and push them to the q
+  void markGridTwo(vector<vector<int>>& grid, int i, int j,
+                   queue<pair<int, int>>& q) {
     if (i < 0 || i == grid.size() || j < 0 || j == grid[0].size())
       return;
     if (grid[i][j] != 1)
       return;
 
     grid[i][j] = 2;
-    markGridTwo(grid, i + 1, j);
-    markGridTwo(grid, i - 1, j);
-    markGridTwo(grid, i, j + 1);
-    markGridTwo(grid, i, j - 1);
-  }
-
-  // Expand from colors' group to 1s' group
-  bool expand(vector<vector<int>>& grid, int i, int j, int color) {
-    if (i < 0 || i == grid.size() || j < 0 || j == grid[0].size())
-      return false;
-    if (grid[i][j] == 0)
-      grid[i][j] = color + 1;
-    return grid[i][j] == 1;  // We touch the 1s' group!
+    q.emplace(i, j);
+    markGridTwo(grid, i + 1, j, q);
+    markGridTwo(grid, i - 1, j, q);
+    markGridTwo(grid, i, j + 1, q);
+    markGridTwo(grid, i, j - 1, q);
   }
 };
