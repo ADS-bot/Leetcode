89417e4255f473323023357402c5a2c3533e9e4f
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-22 18:19:06 +0530

    Time: 16 ms (92.31%), Space: 43.6 MB (51.92%) - LeetHub

--- 0993-tallest-billboard/0993-tallest-billboard.java
+++ 0993-tallest-billboard/0993-tallest-billboard.java
@@ -1,27 +1,26 @@
 class Solution {
   public int tallestBillboard(int[] rods) {
-    final int n = rods.length;
     final int sum = Arrays.stream(rods).sum();
-    // dp[i][j] := max min-height of using rods[0..i) to pile two piles that
-    // Have height difference j
-    int[][] dp = new int[n + 1][sum + 1];
-    Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
-    dp[0][0] = 0;
+    // dp[i] := max min-height of using rods so far to pile two piles that have
+    // Height difference i
+    int[] dp = new int[sum + 1];
+    Arrays.fill(dp, -1);
+    dp[0] = 0;
 
-    for (int i = 1; i <= n; ++i) {
-      final int h = rods[i - 1];
-      for (int j = 0; j <= sum - h; ++j) {
-        if (dp[i - 1][j] < 0)
+    for (final int h : rods) {
+      int[] prev = dp.clone();
+      for (int i = 0; i <= sum - h; ++i) {
+        if (prev[i] < 0)
           continue;
-        // don't use rods[i - 1]
-        dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+        // don't use this rod
+        dp[i] = Math.max(dp[i], prev[i]);
         // Put on the taller pile
-        dp[i][j + h] = Math.max(dp[i][j + h], dp[i - 1][j]);
+        dp[i + h] = Math.max(dp[i + h], prev[i]);
         // Put on the shorter pile
-        dp[i][Math.abs(j - h)] = Math.max(dp[i][Math.abs(j - h)], dp[i - 1][j] + Math.min(j, h));
+        dp[Math.abs(i - h)] = Math.max(dp[Math.abs(i - h)], prev[i] + Math.min(i, h));
       }
     }
 
-    return dp[n][0];
+    return dp[0];
   }
 }
