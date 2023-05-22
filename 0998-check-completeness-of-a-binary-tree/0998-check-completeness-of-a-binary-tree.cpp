db042f351cd215184fc5aa3744b6b4d2cd1598a0
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-22 18:20:11 +0530

    Time: 11 ms (6.9%), Space: 10.3 MB (93.34%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
@@ -1,21 +1,25 @@
 class Solution {
  public:
   bool isCompleteTree(TreeNode* root) {
+    const int count = getCount(root);
+    return validIndex(root, 1, count);
+  }
+
+ private:
+  // Calculate the # of nodes
+  int getCount(TreeNode* root) {
+    if (root == nullptr)
+      return 0;
+    return 1 + getCount(root->left) + getCount(root->right);
+  }
+
+  // Make sure no index is > the # of nodes
+  bool validIndex(TreeNode* root, int index, int count) {
     if (root == nullptr)
       return true;
-
-    queue<TreeNode*> q{{root}};
-
-    while (q.front() != nullptr) {
-      TreeNode* node = q.front();
-      q.pop();
-      q.push(node->left);
-      q.push(node->right);
-    }
-
-    while (!q.empty() && q.front() == nullptr)
-      q.pop();
-
-    return q.empty();
+    if (index > count)
+      return false;
+    return validIndex(root->left, index * 2, count) &&
+           validIndex(root->right, index * 2 + 1, count);
   }
 };
