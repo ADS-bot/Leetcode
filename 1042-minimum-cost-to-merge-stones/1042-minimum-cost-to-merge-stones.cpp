4a75f348a74441f48f052e391ee23f828675c535
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-26 12:56:18 +0530

    Time: 0 ms (100%), Space: 8.2 MB (64.11%) - LeetHub

--- 1042-minimum-cost-to-merge-stones/1042-minimum-cost-to-merge-stones.cpp
+++ 1042-minimum-cost-to-merge-stones/1042-minimum-cost-to-merge-stones.cpp
@@ -1,40 +1,30 @@
 class Solution {
  public:
-  int mergeStones(vector<int>& stones, int k) {
+  int mergeStones(vector<int>& stones, int K) {
     const int n = stones.size();
-    if ((n - 1) % (k - 1))
+    if ((n - 1) % (K - 1))
       return -1;
 
-    this->k = k;
+    constexpr int kMax = 1'000'000'000;
 
     // dp[i][j] := min cost to merge stones[i..j]
-    dp.resize(n, vector<int>(n, kMax));
-    prefix.resize(n + 1);
+    vector<vector<int>> dp(n, vector<int>(n, kMax));
+    vector<int> prefix(n + 1);
+
+    for (int i = 0; i < n; ++i)
+      dp[i][i] = 0;
 
     partial_sum(begin(stones), end(stones), begin(prefix) + 1);
 
-    const int cost = mergeStones(stones, 0, n - 1);
-    return cost == kMax ? -1 : cost;
-  }
+    for (int d = 1; d < n; ++d)
+      for (int i = 0; i + d < n; ++i) {
+        const int j = i + d;
+        for (int m = i; m < j; m += K - 1)
+          dp[i][j] = min(dp[i][j], dp[i][m] + dp[m + 1][j]);
+        if (d % (K - 1) == 0)
+          dp[i][j] += prefix[j + 1] - prefix[i];
+      }
 
- private:
-  static constexpr int kMax = 1'000'000'000;
-  int k;
-  vector<vector<int>> dp;
-  vector<int> prefix;
-
-  int mergeStones(const vector<int>& stones, int i, int j) {
-    if (j - i + 1 < k)
-      return 0;
-    if (dp[i][j] != kMax)
-      return dp[i][j];
-
-    for (int m = i; m < j; m += k - 1)
-      dp[i][j] = min(dp[i][j],
-                     mergeStones(stones, i, m) + mergeStones(stones, m + 1, j));
-    if ((j - i) % (k - 1) == 0)
-      dp[i][j] += prefix[j + 1] - prefix[i];
-
-    return dp[i][j];
+    return dp[0][n - 1];
   }
 };
