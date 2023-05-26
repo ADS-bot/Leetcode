010a43e127fb48e18a8b991b02d8abc13004197d
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-26 12:57:01 +0530

    Time: 4 ms (33.33%), Space: 43.4 MB (7.64%) - LeetHub

--- 1042-minimum-cost-to-merge-stones/1042-minimum-cost-to-merge-stones.java
+++ 1042-minimum-cost-to-merge-stones/1042-minimum-cost-to-merge-stones.java
@@ -1,32 +1,40 @@
 class Solution {
   public int mergeStones(int[] stones, int K) {
     final int n = stones.length;
-    if ((n - 1) % (K - 1) != 0)
-      return -1;
-
-    final int kMax = 1_000_000_000;
+    this.K = K;
 
     // dp[i][j][k] := min cost to merge stones[i..j] into k piles
-    int[][][] dp = new int[n][n][K + 1];
+    dp = new int[n][n][K + 1];
     for (int[][] A : dp)
       Arrays.stream(A).forEach(a -> Arrays.fill(a, kMax));
-    int[] prefix = new int[n + 1];
-
-    for (int i = 0; i < n; ++i)
-      dp[i][i][1] = 0;
+    prefix = new int[n + 1];
 
     for (int i = 0; i < n; ++i)
       prefix[i + 1] = prefix[i] + stones[i];
 
-    for (int d = 1; d < n; ++d)
-      for (int i = 0; i + d < n; ++i) {
-        final int j = i + d;
-        for (int k = 2; k <= K; ++k) // Piles
-          for (int m = i; m < j; m += K - 1)
-            dp[i][j][k] = Math.min(dp[i][j][k], dp[i][m][1] + dp[m + 1][j][k - 1]);
-        dp[i][j][1] = dp[i][j][K] + prefix[j + 1] - prefix[i];
-      }
+    final int cost = mergeStones(stones, 0, n - 1, 1);
+    return cost == kMax ? -1 : cost;
+  }
 
-    return dp[0][n - 1][1];
+  private static final int kMax = 1_000_000_000;
+  private int K;
+  private int[][][] dp;
+  private int[] prefix;
+
+  private int mergeStones(final int[] stones, int i, int j, int k) {
+    if ((j - i + 1 - k) % (K - 1) != 0)
+      return kMax;
+    if (i == j)
+      return k == 1 ? 0 : kMax;
+    if (dp[i][j][k] != kMax)
+      return dp[i][j][k];
+    if (k == 1)
+      return mergeStones(stones, i, j, K) + prefix[j + 1] - prefix[i];
+
+    for (int m = i; m < j; m += K - 1)
+      dp[i][j][k] = Math.min(dp[i][j][k],
+                             mergeStones(stones, i, m, 1) + mergeStones(stones, m + 1, j, k - 1));
+
+    return dp[i][j][k];
   }
 }
