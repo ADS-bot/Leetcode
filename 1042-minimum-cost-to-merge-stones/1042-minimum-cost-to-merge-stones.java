988da2c0b56f115781f4628813c369a888034b6b
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-26 12:56:51 +0530

    Time: 6 ms (15.28%), Space: 41.9 MB (37.5%) - LeetHub

--- 1042-minimum-cost-to-merge-stones/1042-minimum-cost-to-merge-stones.java
+++ 1042-minimum-cost-to-merge-stones/1042-minimum-cost-to-merge-stones.java
@@ -1,39 +1,32 @@
 class Solution {
-  public int mergeStones(int[] stones, int k) {
+  public int mergeStones(int[] stones, int K) {
     final int n = stones.length;
-    if ((n - 1) % (k - 1) != 0)
+    if ((n - 1) % (K - 1) != 0)
       return -1;
 
-    this.k = k;
+    final int kMax = 1_000_000_000;
 
-    // dp[i][j] := min cost to merge stones[i..j]
-    dp = new int[n][n];
-    Arrays.stream(dp).forEach(A -> Arrays.fill(A, kMax));
-    prefix = new int[n + 1];
+    // dp[i][j][k] := min cost to merge stones[i..j] into k piles
+    int[][][] dp = new int[n][n][K + 1];
+    for (int[][] A : dp)
+      Arrays.stream(A).forEach(a -> Arrays.fill(a, kMax));
+    int[] prefix = new int[n + 1];
+
+    for (int i = 0; i < n; ++i)
+      dp[i][i][1] = 0;
 
     for (int i = 0; i < n; ++i)
       prefix[i + 1] = prefix[i] + stones[i];
 
-    final int cost = mergeStones(stones, 0, n - 1);
-    return cost == kMax ? -1 : cost;
-  }
+    for (int d = 1; d < n; ++d)
+      for (int i = 0; i + d < n; ++i) {
+        final int j = i + d;
+        for (int k = 2; k <= K; ++k) // Piles
+          for (int m = i; m < j; m += K - 1)
+            dp[i][j][k] = Math.min(dp[i][j][k], dp[i][m][1] + dp[m + 1][j][k - 1]);
+        dp[i][j][1] = dp[i][j][K] + prefix[j + 1] - prefix[i];
+      }
 
-  private static final int kMax = 1_000_000_000;
-  private int k;
-  private int[][] dp;
-  private int[] prefix;
-
-  private int mergeStones(final int[] stones, int i, int j) {
-    if (j - i + 1 < k)
-      return 0;
-    if (dp[i][j] != kMax)
-      return dp[i][j];
-
-    for (int m = i; m < j; m += k - 1)
-      dp[i][j] = Math.min(dp[i][j], mergeStones(stones, i, m) + mergeStones(stones, m + 1, j));
-    if ((j - i) % (k - 1) == 0)
-      dp[i][j] += prefix[j + 1] - prefix[i];
-
-    return dp[i][j];
+    return dp[0][n - 1][1];
   }
 }
