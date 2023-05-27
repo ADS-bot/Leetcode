4d8d482ff2cceef81afef1f273477dab22d8e503
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-27 12:40:04 +0530

    Time: 54 ms (40.7%), Space: 63.5 MB (5.03%) - LeetHub

--- 1522-stone-game-iii/1522-stone-game-iii.java
+++ 1522-stone-game-iii/1522-stone-game-iii.java
@@ -1,19 +1,27 @@
 class Solution {
   public String stoneGameIII(int[] stoneValue) {
-    final int n = stoneValue.length;
     // dp[i] := max "relative score" Alice can make w/ stoneValue[i:]
-    int[] dp = new int[n + 1];
-    Arrays.fill(dp, 0, n, Integer.MIN_VALUE / 2);
+    dp = new int[stoneValue.length];
+    Arrays.fill(dp, Integer.MIN_VALUE);
 
-    for (int i = n - 1; i >= 0; --i) {
-      int sum = 0;
-      for (int j = i; j < i + 3 && j < n; ++j) {
-        sum += stoneValue[j];
-        dp[i] = Math.max(dp[i], sum - dp[j + 1]);
-      }
-    }
-
-    final int score = dp[0];
+    final int score = stoneGameIII(stoneValue, 0);
     return score > 0 ? "Alice" : score < 0 ? "Bob" : "Tie";
   }
+
+  private int[] dp;
+
+  private int stoneGameIII(int[] stoneValue, int i) {
+    if (i == stoneValue.length)
+      return 0;
+    if (dp[i] > Integer.MIN_VALUE)
+      return dp[i];
+
+    int sum = 0;
+    for (int j = i; j < i + 3 && j < stoneValue.length; ++j) {
+      sum += stoneValue[j];
+      dp[i] = Math.max(dp[i], sum - stoneGameIII(stoneValue, j + 1));
+    }
+
+    return dp[i];
+  };
 }
