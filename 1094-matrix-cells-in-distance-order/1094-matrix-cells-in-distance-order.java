47a97dc1e9d41a190fbb3c40f8d1fe6355b2caed
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-28 16:38:36 +0530

    Time: 8 ms (93.89%), Space: 45.8 MB (8.3%) - LeetHub

--- 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.java
+++ 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.java
@@ -1,34 +1,25 @@
 class Solution {
-    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
-        List<int[]> allCellsList = new ArrayList<int[]>();
-        final int WHITE = 0;
-        final int GRAY = 1;
-        final int BLACK = 2;
-        int[][] colors = new int[R][C];
-        colors[r0][c0] = GRAY;
-        Queue<int[]> queue = new LinkedList<int[]>();
-        queue.offer(new int[]{r0, c0});
-        int[][] directions = { {-1, 0}, {0, -1}, {1, 0}, {0, 1} };
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            allCellsList.add(cell);
-            int row = cell[0], column = cell[1];
-            for (int[] direction : directions) {
-                int newRow = row + direction[0], newColumn = column + direction[1];
-                if (newRow >= 0 && newRow < R && newColumn >= 0 && newColumn < C && colors[newRow][newColumn] == WHITE) {
-                    colors[newRow][newColumn] = GRAY;
-                    queue.offer(new int[]{newRow, newColumn});
+    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{rCenter, cCenter});
+        boolean[][] vis = new boolean[rows][cols];
+        vis[rCenter][cCenter] = true;
+        int[][] ans = new int[rows * cols][2];
+        int[] dirs = {-1, 0, 1, 0, -1};
+        int idx = 0;
+        while (!q.isEmpty()) {
+            for (int n = q.size(); n > 0; --n) {
+                var p = q.poll();
+                ans[idx++] = p;
+                for (int k = 0; k < 4; ++k) {
+                    int x = p[0] + dirs[k], y = p[1] + dirs[k + 1];
+                    if (x >= 0 && x < rows && y >= 0 && y < cols && !vis[x][y]) {
+                        vis[x][y] = true;
+                        q.offer(new int[]{x, y});
+                    }
                 }
             }
-            colors[row][column] = BLACK;
         }
-        int length = allCellsList.size();
-        int[][] allCellsArray = new int[length][2];
-        for (int i = 0; i < length; i++) {
-            int[] cell = allCellsList.get(i);
-            allCellsArray[i][0] = cell[0];
-            allCellsArray[i][1] = cell[1];
-        }
-        return allCellsArray;
+        return ans;
     }
 }
