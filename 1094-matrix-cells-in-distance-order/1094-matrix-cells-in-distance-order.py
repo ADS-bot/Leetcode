6debf5008d20e7508fa218f99ed164e37e0f1859
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-05-28 16:39:24 +0530

    Time: 187 ms (20.55%), Space: 18.9 MB (15.64%) - LeetHub

--- 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.py
+++ 1094-matrix-cells-in-distance-order/1094-matrix-cells-in-distance-order.py
@@ -1,18 +1,15 @@
-class Solution:
-    def allCellsDistOrder(
-        self, rows: int, cols: int, rCenter: int, cCenter: int
-    ) -> List[List[int]]:
-        q = deque([(rCenter, cCenter)])
-        vis = [[False] * cols for _ in range(rows)]
-        vis[rCenter][cCenter] = True
-        ans = []
-        while q:
-            for _ in range(len(q)):
-                i, j = q.popleft()
-                ans.append([i, j])
-                for a, b in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
-                    x, y = i + a, j + b
-                    if 0 <= x < rows and 0 <= y < cols and not vis[x][y]:
-                        q.append((x, y))
-                        vis[x][y] = True
-        return ans
+class Solution(object):
+    def allCellsDistOrder(self, R, C, r0, c0):
+        """
+        :type R: int
+        :type C: int
+        :type r0: int
+        :type c0: int
+        :rtype: List[List[int]]
+        """
+        dis = []
+        for r in range(R):
+            for c in range(C):
+                dis.append((abs(r0 - r) + abs(c0 - c), [r, c]))
+        dis.sort()
+        return [x for d, x in dis]
