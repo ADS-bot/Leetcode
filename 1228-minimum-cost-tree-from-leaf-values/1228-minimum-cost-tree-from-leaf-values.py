184cb3a620565fcaad263516d6480a08b2ab59c9
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-06-10 11:40:50 +0530

    Time: 46 ms (69.58%), Space: 16.4 MB (32.42%) - LeetHub

--- 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.py
+++ 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.py
@@ -1,25 +1,9 @@
 class Solution:
   def mctFromLeafValues(self, arr: List[int]) -> int:
-    n = len(arr)
-    # dp[i][j] := min cost of arr[i..j]
-    dp = [[0] * n for _ in range(n)]
-    # maxVal[i][j] := max value of arr[i..j]
-    maxVal = [[0] * n for _ in range(n)]
+    ans = 0
 
-    for i in range(n):
-      maxVal[i][i] = arr[i]
+    while len(arr) > 1:
+      i = arr.index(min(arr))
+      ans += min(arr[i - 1:i] + arr[i + 1:i + 2]) * arr.pop(i)
 
-    for d in range(1, n):
-      for i in range(n - d):
-        j = i + d
-        maxVal[i][j] = max(maxVal[i][j - 1], maxVal[i + 1][j])
-
-    for d in range(1, n):
-      for i in range(n - d):
-        j = i + d
-        dp[i][j] = math.inf
-        for k in range(i, j):
-          dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] +
-                         maxVal[i][k] * maxVal[k + 1][j])
-
-    return dp[0][-1]
+    return ans
