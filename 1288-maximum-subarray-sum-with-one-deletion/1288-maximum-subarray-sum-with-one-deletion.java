1257f6dc31c0ad41a8447822042728e2d2d5e8eb
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-06-14 11:15:52 +0530

    Time: 5 ms (62.41%), Space: 51.8 MB (45.19%) - LeetHub

--- 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.java
+++ 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.java
@@ -1,17 +1,17 @@
 class Solution {
   // Very similar to 53. Maximum Subarray
   public int maximumSum(int[] arr) {
-    // dp[0][i] := max sum subarray ending w/ i (no deletion)
-    // dp[1][i] := max sum subarray ending w/ i (at most 1 deletion)
-    int[][] dp = new int[2][arr.length];
+    final int kMin = Integer.MIN_VALUE / 2;
+    int ans = kMin;
+    int zero = kMin; // No deletion.
+    int one = kMin;  // At most 1 deletion.
 
-    dp[0][0] = arr[0];
-    dp[1][0] = arr[0];
-    for (int i = 1; i < arr.length; ++i) {
-      dp[0][i] = Math.max(arr[i], dp[0][i - 1] + arr[i]);
-      dp[1][i] = Math.max(arr[i], Math.max(dp[1][i - 1] + arr[i], dp[0][i - 1] /*delete arr[i]*/));
+    for (final int a : arr) {
+      one = Math.max(a, Math.max(one + a, zero /*delete a*/));
+      zero = Math.max(a, zero + a);
+      ans = Math.max(ans, one);
     }
 
-    return Arrays.stream(dp[1]).max().getAsInt();
+    return ans;
   }
 }
