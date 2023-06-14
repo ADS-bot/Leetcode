64969e4097f5df3f6a0fc6a1278c0f1ba86060c9
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-06-14 11:18:06 +0530

    Time: 0 ms (100%), Space: 6.2 MB (74.26%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,19 +1,26 @@
 class Solution {
  public:
   string reverseParentheses(string s) {
-    stack<int> stack;
     string ans;
+    stack<int> stack;
+    unordered_map<int, int> pair;
 
-    for (const char c : s)
-      if (c == '(') {
-        stack.push(ans.length());
-      } else if (c == ')') {
-        // Reverse the corresponding substring between ().
+    for (int i = 0; i < s.length(); ++i)
+      if (s[i] == '(') {
+        stack.push(i);
+      } else if (s[i] == ')') {
         const int j = stack.top();
         stack.pop();
-        reverse(ans.begin() + j, ans.end());
+        pair[i] = j;
+        pair[j] = i;
+      }
+
+    for (int i = 0, d = 1; i < s.length(); i += d)
+      if (s[i] == '(' || s[i] == ')') {
+        i = pair[i];
+        d = -d;
       } else {
-        ans += c;
+        ans += s[i];
       }
 
     return ans;
