64680397b0877ec947c9778894aa0e95919f5c1d
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-06-17 10:27:11 +0530

    Time: 3 ms (97.54%), Space: 8.2 MB (88.42%) - LeetHub

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/1360-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,30 +1,37 @@
 class Solution {
  public:
   int maxLength(vector<string>& arr) {
-    vector<bitset<26>> masks;
+    vector<int> masks;
 
     for (const string& s : arr) {
-      const bitset<26> mask = getMask(s);
-      if (mask.count() == s.length())
+      const int mask = getMask(s);
+      if (mask != -1)
         masks.push_back(mask);
     }
 
-    return dfs(masks, 0, /*usedMask=*/bitset<26>());
+    return dfs(masks, 0, /*usedMask=*/0);
   }
 
  private:
-  int dfs(const vector<bitset<26>>& masks, int s, bitset<26> usedMask) {
-    int res = usedMask.count();
-    for (int i = s; i < masks.size(); ++i)
-      if (!(usedMask & masks[i]).any())
-        res = max(res, dfs(masks, i + 1, usedMask | masks[i]));
-    return res;
+  int dfs(const vector<int>& masks, int i, int usedMask) {
+    if (i == masks.size())
+      return 0;
+    const int pick = (masks[i] & usedMask) == 0
+                         ? __builtin_popcount(masks[i]) +
+                               dfs(masks, i + 1, usedMask | masks[i])
+                         : 0;
+    const int skip = dfs(masks, i + 1, usedMask);
+    return max(pick, skip);
   }
 
-  bitset<26> getMask(const string& s) {
-    bitset<26> mask;
-    for (const char c : s)
-      mask.set(c - 'a');
+  int getMask(const string& s) {
+    int mask = 0;
+    for (const char c : s) {
+      const int i = c - 'a';
+      if ((mask & (1 << i)) != 0)
+        return -1;
+      mask |= 1 << i;
+    }
     return mask;
   }
 };
