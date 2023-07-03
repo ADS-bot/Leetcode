e65f701ce793820682ab01f19b8562e3236516a2
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-07-03 23:55:01 +0530

    Time: 25 ms (74.42%), Space: 8.4 MB (77.26%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -3,15 +3,12 @@ class Solution {
   // Similar to 3. Longest Substring Without Repeating Characters
   int numberOfSubstrings(string s) {
     int ans = 0;
-    vector<int> count(3);
+    vector<int> lastSeen(3, -1);  // lastSeen[c] := index of last c appeared.
 
-    int l = 0;
-    for (const char c : s) {
-      ++count[c - 'a'];
-      while (count[0] > 0 && count[1] > 0 && count[2] > 0)
-        --count[s[l++] - 'a'];
-      // s[0..r], s[1..r], ..., s[l - 1..r] are satified strings.
-      ans += l;
+    for (int i = 0; i < s.length(); ++i) {
+      lastSeen[s[i] - 'a'] = i;
+      // s[0..i], s[1..i], s[min(lastSeen)..i] are satisfied strings.
+      ans += 1 + *min_element(lastSeen.begin(), lastSeen.end());
     }
 
     return ans;
