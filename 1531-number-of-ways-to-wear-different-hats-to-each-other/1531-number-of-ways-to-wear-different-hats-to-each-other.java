6df1352706b7ea622fafeb33452d75e59975debb
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-07-11 10:04:27 +0530

    Time: 29 ms (58.46%), Space: 43.8 MB (50.77%) - LeetHub

--- 1531-number-of-ways-to-wear-different-hats-to-each-other/1531-number-of-ways-to-wear-different-hats-to-each-other.java
+++ 1531-number-of-ways-to-wear-different-hats-to-each-other/1531-number-of-ways-to-wear-different-hats-to-each-other.java
@@ -1,13 +1,10 @@
 class Solution {
   public int numberWays(List<List<Integer>> hats) {
-    final int kMod = 1_000_000_007;
     final int nHats = 40;
     final int nPeople = hats.size();
-    final int nAssignments = 1 << nPeople;
-    List<Integer>[] hatToPeople = new List[nHats + 1];
+    hatToPeople = new List[nHats + 1];
     // dp[i][j] := # of ways to assign hats 1, 2, ..., i to people in mask j
-    int[][] dp = new int[nHats + 1][nAssignments];
-    dp[0][0] = 1;
+    dp = new Integer[nHats + 1][1 << nPeople];
 
     for (int i = 1; i <= nHats; ++i)
       hatToPeople[i] = new ArrayList<>();
@@ -16,22 +13,35 @@ class Solution {
       for (final int hat : hats.get(i))
         hatToPeople[hat].add(i);
 
-    for (int h = 1; h <= nHats; ++h)
-      // For each assignment j of people
-      for (int j = 0; j < nAssignments; ++j) {
-        // We can cover the assignment j in 2 ways:
-        // (1) by first h - 1 hats (i.e., w/o hat h)
-        dp[h][j] += dp[h - 1][j];
-        dp[h][j] %= kMod;
-        for (final int p : hatToPeople[h])
-          if ((j & 1 << p) > 0) {
-            // (2) by first h - 1 hats assigned to people w/o person p and
-            //     hat h assigned to person p
-            dp[h][j] += dp[h - 1][j ^ 1 << p];
-            dp[h][j] %= kMod;
-          }
-      }
+    return ways(hats, 0, 1);
+  }
 
-    return dp[nHats][nAssignments - 1];
+  private static final int kMod = 1_000_000_007;
+  private List<Integer>[] hatToPeople;
+  private Integer[][] dp;
+
+  private int ways(List<List<Integer>> hats, int assignment, int h) {
+    // All people are assigned
+    if (assignment == (1 << hats.size()) - 1)
+      return 1;
+    if (h > 40)
+      return 0;
+    if (dp[h][assignment] != null)
+      return dp[h][assignment];
+
+    // don't wear hat h
+    int ans = ways(hats, assignment, h + 1);
+
+    for (final int p : hatToPeople[h]) {
+      // Person p was assigned hat h before
+      if ((assignment & 1 << p) > 0)
+        continue;
+
+      // Assigned hat h to person p
+      ans += ways(hats, assignment | 1 << p, h + 1);
+      ans %= kMod;
+    }
+
+    return dp[h][assignment] = ans;
   }
 }
