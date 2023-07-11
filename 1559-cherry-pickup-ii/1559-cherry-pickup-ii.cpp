2775d20b3a970c65fbfbf6b7a1acebe05aabc4da
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-07-11 10:20:10 +0530

    Time: 317 ms (5.75%), Space: 17 MB (15.27%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,34 +1,22 @@
 class Solution {
  public:
   int cherryPickup(vector<vector<int>>& grid) {
-    this->m = grid.size();
-    this->n = grid[0].size();
+    const int m = grid.size();
+    const int n = grid[0].size();
     // dp[x][y1][y2] := max cherries we can collect w/
     // Robot #1 on (x, y1) and Robot #2 on (x, y2) first
-    dp.resize(m, vector<vector<int>>(n, vector<int>(n, -1)));
-    return cherryPick(grid, 0, 0, n - 1);
-  }
+    vector<vector<vector<int>>> dp(m + 1,
+                                   vector<vector<int>>(n, vector<int>(n)));
 
- private:
-  int m;
-  int n;
-  vector<vector<vector<int>>> dp;
+    for (int x = m - 1; x >= 0; --x)
+      for (int y1 = 0; y1 < n; ++y1)
+        for (int y2 = 0; y2 < n; ++y2) {
+          const int currRow = grid[x][y1] + (y1 != y2) * grid[x][y2];
+          for (int d1 = max(0, y1 - 1); d1 < min(n, y1 + 2); ++d1)
+            for (int d2 = max(0, y2 - 1); d2 < min(n, y2 + 2); ++d2)
+              dp[x][y1][y2] = max(dp[x][y1][y2], currRow + dp[x + 1][d1][d2]);
+        }
 
-  int cherryPick(const vector<vector<int>>& grid, int x, int y1, int y2) {
-    if (x == m)
-      return 0;
-    if (y1 < 0 || y1 == n || y2 < 0 || y2 == n)
-      return 0;
-    if (dp[x][y1][y2] != -1)
-      return dp[x][y1][y2];
-
-    const int currRow = grid[x][y1] + (y1 != y2) * grid[x][y2];
-
-    for (int d1 = -1; d1 <= 1; ++d1)
-      for (int d2 = -1; d2 <= 1; ++d2)
-        dp[x][y1][y2] = max(
-            dp[x][y1][y2], currRow + cherryPick(grid, x + 1, y1 + d1, y2 + d2));
-
-    return dp[x][y1][y2];
+    return dp[0][0][n - 1];
   }
 };
