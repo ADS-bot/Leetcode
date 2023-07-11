6bb6a9952fc2bdf3cfbc7514ee3dd912c4bac694
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-07-11 10:02:19 +0530

    Time: 28 ms (93.02%), Space: 11.6 MB (32.24%) - LeetHub

--- 1530-check-if-a-string-can-break-another-string/1530-check-if-a-string-can-break-another-string.cpp
+++ 1530-check-if-a-string-can-break-another-string/1530-check-if-a-string-can-break-another-string.cpp
@@ -1,28 +1,18 @@
 class Solution {
  public:
   bool checkIfCanBreak(string s1, string s2) {
-    vector<int> count1(26);
-    vector<int> count2(26);
+    vector<int> count(26);
 
     for (const char c : s1)
-      ++count1[c - 'a'];
+      ++count[c - 'a'];
 
     for (const char c : s2)
-      ++count2[c - 'a'];
+      --count[c - 'a'];
 
-    return canBreak(count1, count2) || canBreak(count2, count1);
-  }
+    for (int i = 1; i < 26; ++i)
+      count[i] += count[i - 1];
 
- private:
-  // Returns True if count1 can break count2.
-  bool canBreak(const vector<int>& count1, const vector<int>& count2) {
-    int diff = 0;
-    for (int i = 0; i < 26; ++i) {
-      diff += count2[i] - count1[i];
-      // count2 is alphabetically greater than count1.
-      if (diff < 0)
-        return false;
-    }
-    return true;
+    return all_of(count.begin(), count.end(), [](int c) { return c <= 0; }) ||
+           all_of(count.begin(), count.end(), [](int c) { return c >= 0; });
   }
 };
