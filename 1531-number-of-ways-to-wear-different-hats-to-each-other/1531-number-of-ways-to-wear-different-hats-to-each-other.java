9affb89bde3c91b737bb6853d727f5c1841ff436
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-07-11 10:04:17 +0530

    Time: 30 ms (53.85%), Space: 44.1 MB (27.69%) - LeetHub

--- 1531-number-of-ways-to-wear-different-hats-to-each-other/1531-number-of-ways-to-wear-different-hats-to-each-other.java
+++ 1531-number-of-ways-to-wear-different-hats-to-each-other/1531-number-of-ways-to-wear-different-hats-to-each-other.java
@@ -5,9 +5,9 @@ class Solution {
     final int nPeople = hats.size();
     final int nAssignments = 1 << nPeople;
     List<Integer>[] hatToPeople = new List[nHats + 1];
-    // dp[i] := # of ways to assign hats so far to people in mask i
-    int[] dp = new int[nAssignments];
-    dp[0] = 1;
+    // dp[i][j] := # of ways to assign hats 1, 2, ..., i to people in mask j
+    int[][] dp = new int[nHats + 1][nAssignments];
+    dp[0][0] = 1;
 
     for (int i = 1; i <= nHats; ++i)
       hatToPeople[i] = new ArrayList<>();
@@ -17,13 +17,21 @@ class Solution {
         hatToPeople[hat].add(i);
 
     for (int h = 1; h <= nHats; ++h)
-      for (int j = nAssignments - 1; j >= 0; --j)
+      // For each assignment j of people
+      for (int j = 0; j < nAssignments; ++j) {
+        // We can cover the assignment j in 2 ways:
+        // (1) by first h - 1 hats (i.e., w/o hat h)
+        dp[h][j] += dp[h - 1][j];
+        dp[h][j] %= kMod;
         for (final int p : hatToPeople[h])
           if ((j & 1 << p) > 0) {
-            dp[j] += dp[j ^ 1 << p];
-            dp[j] %= kMod;
+            // (2) by first h - 1 hats assigned to people w/o person p and
+            //     hat h assigned to person p
+            dp[h][j] += dp[h - 1][j ^ 1 << p];
+            dp[h][j] %= kMod;
           }
+      }
 
-    return dp[nAssignments - 1];
+    return dp[nHats][nAssignments - 1];
   }
 }
