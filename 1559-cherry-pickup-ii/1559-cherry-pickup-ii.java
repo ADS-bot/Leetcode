1f8e7ead9856f3f7cf3c6abdf87f6918e0cec6af
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-07-11 10:20:31 +0530

    Time: 32 ms (72.16%), Space: 44.5 MB (45.75%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -1,20 +1,34 @@
 class Solution {
   public int cherryPickup(int[][] grid) {
-    final int m = grid.length;
-    final int n = grid[0].length;
+    this.m = grid.length;
+    this.n = grid[0].length;
     // dp[x][y1][y2] := max cherries we can collect w/
     // Robot #1 on (x, y1) and Robot #2 on (x, y2) first
-    int[][][] dp = new int[m + 1][n][n];
+    dp = new int[m][n][n];
+    for (int[][] A : dp)
+      Arrays.stream(A).forEach(a -> Arrays.fill(a, -1));
+    return cherryPick(grid, 0, 0, n - 1);
+  }
 
-    for (int x = m - 1; x >= 0; --x)
-      for (int y1 = 0; y1 < n; ++y1)
-        for (int y2 = 0; y2 < n; ++y2) {
-          final int currRow = grid[x][y1] + (y1 == y2 ? 0 : 1) * grid[x][y2];
-          for (int d1 = Math.max(0, y1 - 1); d1 < Math.min(n, y1 + 2); ++d1)
-            for (int d2 = Math.max(0, y2 - 1); d2 < Math.min(n, y2 + 2); ++d2)
-              dp[x][y1][y2] = Math.max(dp[x][y1][y2], currRow + dp[x + 1][d1][d2]);
-        }
+  private int m;
+  private int n;
+  private int[][][] dp;
 
-    return dp[0][0][n - 1];
+  private int cherryPick(int[][] grid, int x, int y1, int y2) {
+    if (x == m)
+      return 0;
+    if (y1 < 0 || y1 == n || y2 < 0 || y2 == n)
+      return 0;
+    if (dp[x][y1][y2] != -1)
+      return dp[x][y1][y2];
+
+    final int currRow = grid[x][y1] + (y1 == y2 ? 0 : 1) * grid[x][y2];
+
+    for (int d1 = -1; d1 <= 1; ++d1)
+      for (int d2 = -1; d2 <= 1; ++d2)
+        dp[x][y1][y2] =
+            Math.max(dp[x][y1][y2], currRow + cherryPick(grid, x + 1, y1 + d1, y2 + d2));
+
+    return dp[x][y1][y2];
   }
 }
