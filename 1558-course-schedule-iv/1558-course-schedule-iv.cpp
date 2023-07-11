f72c157ff3e3deabdfee49d0698034c46e62e5a3
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-07-11 10:18:10 +0530

    Time: 572 ms (23.1%), Space: 59 MB (99.03%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.cpp
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -4,19 +4,20 @@ class Solution {
                                    vector<vector<int>>& prerequisites,
                                    vector<vector<int>>& queries) {
     vector<bool> ans;
-    vector<vector<int>> graph(numCourses);
     // isPrerequisite[i][j] := true if course i is a prerequisite of course j.
     vector<vector<bool>> isPrerequisite(numCourses, vector<bool>(numCourses));
 
     for (const vector<int>& prerequisite : prerequisites) {
       const int u = prerequisite[0];
       const int v = prerequisite[1];
-      graph[u].push_back(v);
+      isPrerequisite[u][v] = true;
     }
 
-    // DFS from every course.
-    for (int i = 0; i < numCourses; ++i)
-      dfs(graph, i, isPrerequisite[i]);
+    for (int k = 0; k < numCourses; ++k)
+      for (int i = 0; i < numCourses; ++i)
+        for (int j = 0; j < numCourses; ++j)
+          isPrerequisite[i][j] = isPrerequisite[i][j] ||
+                                 (isPrerequisite[i][k] && isPrerequisite[k][j]);
 
     for (const vector<int>& query : queries) {
       const int u = query[0];
@@ -25,14 +26,5 @@ class Solution {
     }
 
     return ans;
-  }
-
-  void dfs(const vector<vector<int>>& graph, int u, vector<bool>& used) {
-    for (const int v : graph[u]) {
-      if (used[v])
-        continue;
-      used[v] = true;
-      dfs(graph, v, used);
-    }
   }
 };
