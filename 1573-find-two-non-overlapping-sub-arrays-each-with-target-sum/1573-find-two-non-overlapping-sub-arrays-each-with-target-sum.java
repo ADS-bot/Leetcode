3a29e1569e1a0f25a72d3abed9af67ab77b19198
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-07-12 09:46:18 +0530

    Time: 128 ms (17.58%), Space: 63.9 MB (7.69%) - LeetHub

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,22 +1,25 @@
 class Solution {
   public int minSumOfLengths(int[] arr, int target) {
     int ans = Integer.MAX_VALUE;
-    int sum = 0; // Window sum
-    // best[i] := min length of subarray of arr[0..i] that has sum = target
-    int[] best = new int[arr.length];
-    Arrays.fill(best, Integer.MAX_VALUE);
+    int leftLength = Integer.MAX_VALUE;
+    int prefix = 0;
+    Map<Integer, Integer> prefixToIndex = new HashMap<>();
+    prefixToIndex.put(0, -1);
 
-    for (int l = 0, r = 0; r < arr.length; ++r) {
-      sum += arr[r]; // Expand window
-      while (sum > target)
-        sum -= arr[l++]; // Shrink window
-      if (sum == target) {
-        if (l > 0 && best[l - 1] != Integer.MAX_VALUE)
-          ans = Math.min(ans, best[l - 1] + r - l + 1);
-        best[r] = Math.min(best[r], r - l + 1);
-      }
-      if (r > 0)
-        best[r] = Math.min(best[r], best[r - 1]);
+    for (int i = 0; i < arr.length; ++i) {
+      prefix += arr[i];
+      prefixToIndex.put(prefix, i);
+    }
+
+    prefix = 0;
+
+    for (int i = 0; i < arr.length; ++i) {
+      prefix += arr[i];
+      if (prefixToIndex.containsKey(prefix - target))
+        leftLength = Math.min(leftLength, i - prefixToIndex.get(prefix - target));
+      if (leftLength < Integer.MAX_VALUE)
+        if (prefixToIndex.containsKey(prefix + target))
+          ans = Math.min(ans, leftLength + prefixToIndex.get(prefix + target) - i);
     }
 
     return ans == Integer.MAX_VALUE ? -1 : ans;
