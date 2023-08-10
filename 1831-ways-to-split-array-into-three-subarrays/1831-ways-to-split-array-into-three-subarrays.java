375dd09eed71707ded7e1e31d5b354d938fb3e06
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-08-10 16:53:24 +0530

    Time: 47 ms (36.07%), Space: 56.1 MB (42.62%) - LeetHub

--- 1831-ways-to-split-array-into-three-subarrays/1831-ways-to-split-array-into-three-subarrays.java
+++ 1831-ways-to-split-array-into-three-subarrays/1831-ways-to-split-array-into-three-subarrays.java
@@ -8,21 +8,48 @@ class Solution {
     for (int i = 1; i < n; ++i)
       prefix[i] += prefix[i - 1];
 
-    for (int i = 0, j = 0, k = 0; i < n - 2; ++i) {
-      // Find the first index j s.t.
-      // Left = prefix[i] <= mid = prefix[j] - prefix[i]
-      j = Math.max(j, i + 1);
-      while (j < n - 1 && prefix[i] > prefix[j] - prefix[i])
-        ++j;
-      // Find the first index k s.t.
-      // Mid = prefix[k] - prefix[i] > right = prefix[-1] - prefix[k]
-      k = Math.max(k, j);
-      while (k < n - 1 && prefix[k] - prefix[i] <= prefix[prefix.length - 1] - prefix[k])
-        ++k;
-      ans += k - j;
-      ans %= kMod;
+    for (int i = 0; i < n - 2; ++i) {
+      final int j = firstGreaterEqual(prefix, i);
+      if (j == n - 1)
+        break;
+      final int mid = prefix[j] - prefix[i];
+      final int right = prefix[prefix.length - 1] - prefix[j];
+      if (mid > right)
+        continue;
+      final int k = firstGreater(prefix, i);
+      ans = (ans + k - j) % kMod;
     }
 
     return ans;
   }
+
+  // Find the first index j s.t.
+  // Mid = prefix[j] - prefix[i] >= left = prefix[i]
+  private int firstGreaterEqual(int[] prefix, int i) {
+    int l = i + 1;
+    int r = prefix.length - 1;
+    while (l < r) {
+      final int m = (l + r) / 2;
+      if (prefix[m] - prefix[i] >= prefix[i])
+        r = m;
+      else
+        l = m + 1;
+    }
+    return l;
+  };
+
+  // Find the first index k s.t.
+  // Mid = prefix[k] - prefix[i] > right = prefix[-1] - prefix[k]
+  private int firstGreater(int[] prefix, int i) {
+    int l = i + 1;
+    int r = prefix.length - 1;
+    while (l < r) {
+      final int m = (l + r) / 2;
+      if (prefix[m] - prefix[i] > prefix[prefix.length - 1] - prefix[m])
+        r = m;
+      else
+        l = m + 1;
+    }
+    return l;
+  };
 }
