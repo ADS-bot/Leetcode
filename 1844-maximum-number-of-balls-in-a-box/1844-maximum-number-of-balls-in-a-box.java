a8260511f207c3212a92ee43dfc21725736b5abf
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-08-21 20:02:36 +0530

    Time: 15 ms (87.36%), Space: 39.3 MB (83.22%) - LeetHub

--- 1844-maximum-number-of-balls-in-a-box/1844-maximum-number-of-balls-in-a-box.java
+++ 1844-maximum-number-of-balls-in-a-box/1844-maximum-number-of-balls-in-a-box.java
@@ -1,22 +1,13 @@
 class Solution {
     public int countBalls(int lowLimit, int highLimit) {
-        int[] sums = new int[50];
-        for (int i = lowLimit; i <= highLimit; i++) {
-            int sum = getSumOfDigits(i);
-            sums[sum]++;
+        int[] cnt = new int[50];
+        for (int i = lowLimit; i <= highLimit; ++i) {
+            int y = 0;
+            for (int x = i; x > 0; x /= 10) {
+                y += x % 10;
+            }
+            ++cnt[y];
         }
-        int maxCount = 0;
-        for (int i = 0; i < 50; i++)
-            maxCount = Math.max(maxCount, sums[i]);
-        return maxCount;
-    }
-
-    public int getSumOfDigits(int num) {
-        int sum = 0;
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
-        }
-        return sum;
+        return Arrays.stream(cnt).max().getAsInt();
     }
 }
