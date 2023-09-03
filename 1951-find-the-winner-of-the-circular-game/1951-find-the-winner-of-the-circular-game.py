ff97ea38a6062b47a3a12ea240d8294184583ce8
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-09-03 17:54:59 +0530

    Time: 48 ms (55.78%), Space: 16.8 MB (17.73%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
@@ -1,21 +1,20 @@
 class Solution:
   def findTheWinner(self, n: int, k: int) -> int:
-    # True if i-th friend is left
-    friends = [False] * n
+    # E.g., n = 4, k = 2.
+    # By using 0-indexed notation, we have the following circle:
+    #
+    # 0 -> 1 -> 2 -> 3 -> 0
+    #      x
+    #           0 -> 1 -> 2 -> 0
+    #
+    # After the first round, 1 is removed.
+    # So, 2 becomes 0, 3 becomes 1, and 0 becomes 2.
+    # Let's denote that oldIndex = f(n, k) and newIndex = f(n - 1, k).
+    # By observation, we know f(n, k) = (f(n - 1, k) + k) % n
+    def f(n: int, k: int) -> int:
+      if n == 1:
+        return 0
+      return (f(n - 1, k) + k) % n
 
-    friendCount = n
-    fp = 0  # friends' pointer
-
-    while friendCount > 1:
-      for _ in range(k):
-        while friends[fp % n]:  # The friend is not there.
-          fp += 1  # Point to the next one.
-        fp += 1
-      friends[(fp - 1) % n] = True
-      friendCount -= 1
-
-    fp = 0
-    while friends[fp]:
-      fp += 1
-
-    return fp + 1
+    # Converts back to 1-indexed
+    return f(n, k) + 1
