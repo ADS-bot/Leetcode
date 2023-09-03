b26eab9987c2147249bdd801e4715937b31f39a5
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-09-03 17:54:30 +0530

    Time: 36 ms (37.97%), Space: 39 MB (88.96%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,25 +1,21 @@
 class Solution {
   public int findTheWinner(int n, int k) {
-    // Converts back to 1-indexed
-    return f(n, k) + 1;
-  }
+    // friends[i] := true if i-th friend is left.
+    boolean[] friends = new boolean[n];
 
-  // E.g., n = 4, k = 2.
-  // By using 0-indexed notation, we have the following circle:
-  //
-  // 0 -> 1 -> 2 -> 3 -> 0
-  //      x
-  //           0 -> 1 -> 2 -> 0
-  //
-  // After the first round, 1 is removed.
-  // So, 2 becomes 0, 3 becomes 1, and 0 becomes 2.
-  // Let's denote that oldIndex = f(n, k) and newIndex = f(n - 1, k).
-  // By observation, we know f(n, k) = (f(n - 1, k) + k) % n
-  private int f(int n, int k) {
-    int ans = 0; // f(1, k)
-    // Computes f(i, k) based on f(i - 1, k)
-    for (int i = 2; i <= n; ++i)
-      ans = (ans + k) % i;
-    return ans;
+    int friendCount = n;
+    int fp = n; // friends' pointer
+
+    while (friendCount > 1) {
+      for (int i = 0; i < k; ++i, ++fp)
+        while (friends[fp % n]) // The friend is not there.
+          ++fp;                 // Point to the next one.
+      friends[(fp - 1) % n] = true;
+      --friendCount;
+    }
+
+    for (fp = 0; friends[fp]; ++fp)
+      ;
+    return fp + 1;
   }
 }
