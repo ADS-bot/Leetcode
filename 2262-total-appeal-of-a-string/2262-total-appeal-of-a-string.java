09eea3c9e1e090315884575e7ca57822b8fca47a
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-12-22 22:17:10 +0530

    Time: 7 ms (79.01%), Space: 45.3 MB (5.76%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
@@ -1,13 +1,18 @@
 class Solution {
   public long appealSum(String s) {
-    final int n = s.length();
     long ans = 0;
+    // the total appeal of all substrings ending in the index so far
+    int dp = 0;
     int[] lastSeen = new int[26];
     Arrays.fill(lastSeen, -1);
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < s.length(); ++i) {
+      //   the total appeal of all substrings ending in s[i]
+      // = the total appeal of all substrings ending in s[i - 1]
+      // + the number of substrings ending in s[i] that contain only this s[i]
       final int c = s.charAt(i) - 'a';
-      ans += (i - lastSeen[c]) * (n - i);
+      dp += i - lastSeen[c];
+      ans += dp;
       lastSeen[c] = i;
     }
 
