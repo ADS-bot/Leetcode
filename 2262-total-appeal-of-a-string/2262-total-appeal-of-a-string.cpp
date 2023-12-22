7f1a47f1395a507e61279ceb0f7e1265790be3a1
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2023-12-22 22:16:41 +0530

    Time: 26 ms (66.73%), Space: 10.9 MB (55.26%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -1,18 +1,13 @@
 class Solution {
  public:
   long long appealSum(string s) {
+    const int n = s.length();
     long long ans = 0;
-    // the total appeal of all substrings ending in the index so far
-    int dp = 0;
     vector<int> lastSeen(26, -1);
 
-    for (int i = 0; i < s.length(); ++i) {
-      //   the total appeal of all substrings ending in s[i]
-      // = the total appeal of all substrings ending in s[i - 1]
-      // + the number of substrings ending in s[i] that contain only this s[i]
+    for (int i = 0; i < n; ++i) {
       const int c = s[i] - 'a';
-      dp += i - lastSeen[c];
-      ans += dp;
+      ans += (i - lastSeen[c]) * (n - i);
       lastSeen[c] = i;
     }
 
