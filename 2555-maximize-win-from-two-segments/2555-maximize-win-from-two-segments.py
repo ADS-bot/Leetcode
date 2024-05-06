3f3230b204621bb9e0b57f7c38730899a1fde7f6
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-05-06 13:08:57 +0530

    Time: 469 ms (81.93%), Space: 27.4 MB (83.13%) - LeetHub

--- 2555-maximize-win-from-two-segments/2555-maximize-win-from-two-segments.py
+++ 2555-maximize-win-from-two-segments/2555-maximize-win-from-two-segments.py
@@ -1,18 +1,16 @@
-class Solution {
-  public int maximizeWin(int[] prizePositions, int k) {
-    int ans = 0;
-    // dp[i] := the maximum number of prizes to choose the first i
-    // `prizePositions`
-    int[] dp = new int[prizePositions.length + 1];
+class Solution:
+  def maximizeWin(self, prizePositions: List[int], k: int) -> int:
+    ans = 0
+    # dp[i] := the maximum number of prizes to choose the first i
+    # `prizePositions`
+    dp = [0] * (len(prizePositions) + 1)
 
-    for (int i = 0, j = 0; i < prizePositions.length; ++i) {
-      while (prizePositions[i] - prizePositions[j] > k)
-        ++j;
-      final int covered = i - j + 1;
-      dp[i + 1] = Math.max(dp[i], covered);
-      ans = Math.max(ans, dp[j] + covered);
-    }
+    j = 0
+    for i, prizePosition in enumerate(prizePositions):
+      while prizePosition - prizePositions[j] > k:
+        j += 1
+      covered = i - j + 1
+      dp[i + 1] = max(dp[i], covered)
+      ans = max(ans, dp[j] + covered)
 
-    return ans;
-  }
-}
+    return ans
