42ff9d25f1d0cfede01b3a10a104a79d85fbed1d
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-05-18 01:11:43 +0530

    Time: 2017 ms (35.57%), Space: 16.7 MB (65.77%) - LeetHub

--- 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.py
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.py
@@ -1,17 +1,14 @@
 class Solution:
   def waysToReachTarget(self, target: int, types: List[List[int]]) -> int:
     kMod = 1_000_000_007
-    # dp[i][j] := the number of ways to earn j points with the first i types
-    dp = [[0] * (target + 1) for _ in range(len(types) + 1)]
-    dp[0][0] = 1
+    # dp[j] := the number of ways to earn j points with the types so far
+    dp = [1] + [0] * target
 
-    for i in range(1, len(types) + 1):
-      count = types[i - 1][0]
-      mark = types[i - 1][1]
-      for j in range(target + 1):
-        for solved in range(count + 1):
+    for count, mark in types:
+      for j in range(target, -1, -1):
+        for solved in range(1, count + 1):
           if j - solved * mark >= 0:
-            dp[i][j] += dp[i - 1][j - solved * mark]
-            dp[i][j] %= kMod
+            dp[j] += dp[j - solved * mark]
+            dp[j] %= kMod
 
-    return dp[len(types)][target]
+    return dp[target]
