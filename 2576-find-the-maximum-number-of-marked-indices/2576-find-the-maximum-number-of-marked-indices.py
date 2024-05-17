03c3e4455258fdae840dea32bd098bab5096708e
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-05-18 01:06:22 +0530

    Time: 486 ms (77.67%), Space: 31.3 MB (66.02%) - LeetHub

--- 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.py
+++ 2576-find-the-maximum-number-of-marked-indices/2576-find-the-maximum-number-of-marked-indices.py
@@ -2,12 +2,11 @@ class Solution:
   def maxNumOfMarkedIndices(self, nums: List[int]) -> int:
     nums.sort()
 
-    def isPossible(m: int) -> bool:
-      for i in range(m):
-        if 2 * nums[i] > nums[-m + i]:
-          return False
-      return True
+    i = 0
+    for j in range(len(nums) // 2, len(nums)):
+      if 2 * nums[i] <= nums[j]:
+        i += 1
+        if i == len(nums) // 2:
+          break
 
-    l = bisect.bisect_left(range(len(nums) // 2 + 1), True,
-                           key=lambda m: not isPossible(m))
-    return (l - 1) * 2
+    return i * 2
