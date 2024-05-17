25c09fa3da5f048504e078b887e3ab4898d85408
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-05-18 01:12:33 +0530

    Time: 66 ms (57.29%), Space: 44.2 MB (54.17%) - LeetHub

--- 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.java
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.java
@@ -1,21 +1,21 @@
 class Solution {
   public int waysToReachTarget(int target, int[][] types) {
     final int kMod = 1_000_000_007;
-    // dp[j] := the number of ways to earn j points with the types so far
-    int[] dp = new int[target + 1];
-    dp[0] = 1;
+    // dp[i][j] := the number of ways to earn j points with the first i types
+    int[][] dp = new int[types.length + 1][target + 1];
+    dp[0][0] = 1;
 
-    for (int[] type : types) {
-      final int count = type[0];
-      final int mark = type[1];
-      for (int j = target; j >= 0; --j)
-        for (int solved = 1; solved <= count; ++solved)
+    for (int i = 1; i <= types.length; ++i) {
+      final int count = types[i - 1][0];
+      final int mark = types[i - 1][1];
+      for (int j = 0; j <= target; ++j)
+        for (int solved = 0; solved <= count; ++solved)
           if (j - solved * mark >= 0) {
-            dp[j] += dp[j - solved * mark];
-            dp[j] %= kMod;
+            dp[i][j] += dp[i - 1][j - solved * mark];
+            dp[i][j] %= kMod;
           }
     }
 
-    return dp[target];
+    return dp[types.length][target];
   }
 }
