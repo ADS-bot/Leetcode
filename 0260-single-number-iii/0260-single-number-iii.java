93dd426edc40c7c58762d9ba5bf1604ada425236
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-05-31 09:48:39 +0530

    Time: 2 ms (48.32%), Space: 45.4 MB (69.87%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,14 +1,16 @@
-class Solution:
-  def singleNumber(self, nums: List[int]) -> List[int]:
-    xors = functools.reduce(operator.xor, nums)
-    lowbit = xors & -xors
-    ans = [0, 0]
+class Solution {
+  public int[] singleNumber(int[] nums) {
+    final int xors = Arrays.stream(nums).reduce((a, b) -> a ^ b).getAsInt();
+    final int lowbit = xors & -xors;
+    int[] ans = new int[2];
 
-    # Seperate `nums` into two groups by `lowbit`.
-    for num in nums:
-      if num & lowbit:
-        ans[0] ^= num
-      else:
-        ans[1] ^= num
+    // Seperate `nums` into two groups by `lowbit`.
+    for (final int num : nums)
+      if ((num & lowbit) > 0)
+        ans[0] ^= num;
+      else
+        ans[1] ^= num;
 
-    return ans
+    return ans;
+  }
+}
