e99dee72598e01d0821044df4163efc936ed9b86
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-08 23:42:16 +0530

    Time: 74 ms (21.59%), Space: 60.9 MB (44.32%) - LeetHub

--- 2659-make-array-empty/2659-make-array-empty.java
+++ 2659-make-array-empty/2659-make-array-empty.java
@@ -1,23 +1,22 @@
 class Solution {
- public:
-  long long countOperationsToEmptyArray(vector<int>& nums) {
-    const int n = nums.size();
+  public long countOperationsToEmptyArray(int[] nums) {
+    final int n = nums.length;
     long ans = n;
-    unordered_map<int, int> numToIndex;
+    Map<Integer, Integer> numToIndex = new HashMap<>();
 
     for (int i = 0; i < n; ++i)
-      numToIndex[nums[i]] = i;
+      numToIndex.put(nums[i], i);
 
-    ranges::sort(nums);
+    Arrays.sort(nums);
 
     for (int i = 1; i < n; ++i)
       // On the i-th step we've already removed the i - 1 smallest numbers and
       // can ignore them. If an element nums[i] has smaller index in origin
       // array than nums[i - 1], we should rotate the whole left array n - i
       // times to set nums[i] element on the first position.
-      if (numToIndex[nums[i]] < numToIndex[nums[i - 1]])
+      if (numToIndex.get(nums[i]) < numToIndex.get(nums[i - 1]))
         ans += n - i;
 
     return ans;
   }
-};
+}
