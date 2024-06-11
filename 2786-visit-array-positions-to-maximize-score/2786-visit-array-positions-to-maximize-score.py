dfac6d9168f40bdb32a7b9afb46e024b22495241
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-12 00:21:19 +0530

    Time: 743 ms (100.00%), Space: 31.6 MB (63.92%) - LeetHub

--- 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.py
+++ 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.py
@@ -1,19 +1,17 @@
-class Solution {
-  public long maxScore(int[] nums, int x) {
-    // Note that we always need to take nums[0], so the initial definition might
-    // not hold true.
+class Solution:
+  def maxScore(self, nums: List[int], x: int) -> int:
+    # Note that we always need to take nums[0], so the initial definition might
+    # not hold true.
 
-    // dp0 := the maximum score so far with `nums` ending in an even number
-    long dp0 = nums[0] - (nums[0] % 2 == 1 ? x : 0);
-    // dp1 := the maximum score so far with `nums` ending in an odd number
-    long dp1 = nums[0] - (nums[0] % 2 == 0 ? x : 0);
+    # dp0 := the maximum score so far with `nums` ending in an even number
+    dp0 = nums[0] - (x if nums[0] % 2 == 1 else 0)
+    # dp0 := the maximum score so far with `nums` ending in an odd number
+    dp1 = nums[0] - (x if nums[0] % 2 == 0 else 0)
 
-    for (int i = 1; i < nums.length; ++i)
-      if (nums[i] % 2 == 0)
-        dp0 = nums[i] + Math.max(dp0, dp1 - x);
-      else
-        dp1 = nums[i] + Math.max(dp1, dp0 - x);
+    for i in range(1, len(nums)):
+      if nums[i] % 2 == 0:
+        dp0 = nums[i] + max(dp0, dp1 - x)
+      else:
+        dp1 = nums[i] + max(dp1, dp0 - x)
 
-    return Math.max(dp0, dp1);
-  }
-}
+    return max(dp0, dp1)
