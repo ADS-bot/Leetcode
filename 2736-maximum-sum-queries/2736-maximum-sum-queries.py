1c07224f11c86cd744f6fd7a9b7ad6fd64ed04d4
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-12 00:19:51 +0530

    Time: 1362 ms (41.38%), Space: 68.5 MB (93.10%) - LeetHub

--- 2736-maximum-sum-queries/2736-maximum-sum-queries.py
+++ 2736-maximum-sum-queries/2736-maximum-sum-queries.py
@@ -1,63 +1,58 @@
-class Solution {
-  public int[] maximumSumQueries(int[] nums1, int[] nums2, int[][] queries) {
-    MyPair[] pairs = getPairs(nums1, nums2);
-    IndexedQuery[] indexedQueries = getIndexedQueries(queries);
-    int[] ans = new int[queries.length];
-    List<Pair<Integer, Integer>> stack = new ArrayList<>(); // [(y, x + y)]
+class Pair:
+  def __init__(self, x: int, y: int):
+    self.x = x
+    self.y = y
 
-    int pairsIndex = 0;
-    for (IndexedQuery indexedQuery : indexedQueries) {
-      final int queryIndex = indexedQuery.queryIndex;
-      final int minX = indexedQuery.minX;
-      final int minY = indexedQuery.minY;
-      while (pairsIndex < pairs.length && pairs[pairsIndex].x >= minX) {
-        MyPair pair = pairs[pairsIndex++];
-        // x + y is a better candidate. Given that x is decreasing, the
-        // condition "x + y >=  stack.get(stack.size() - 1).getValue()" suggests
-        // that y is relatively larger, thereby making it a better candidate.
-        final int x = pair.x;
-        final int y = pair.y;
-        while (!stack.isEmpty() && x + y >= stack.get(stack.size() - 1).getValue())
-          stack.remove(stack.size() - 1);
-        if (stack.isEmpty() || y > stack.get(stack.size() - 1).getKey())
-          stack.add(new Pair<>(y, x + y));
-      }
-      final int j = firstGreaterEqual(stack, minY);
-      ans[queryIndex] = j == stack.size() ? -1 : stack.get(j).getValue();
-    }
+  def __iter__(self):
+    yield self.x
+    yield self.y
 
-    return ans;
-  }
 
-  private record MyPair(int x, int y){};
-  private record IndexedQuery(int queryIndex, int minX, int minY){};
+class IndexedQuery:
+  def __init__(self, queryIndex: int, minX: int, minY: int):
+    self.queryIndex = queryIndex
+    self.minX = minX
+    self.minY = minY
 
-  private int firstGreaterEqual(List<Pair<Integer, Integer>> A, int target) {
-    int l = 0;
-    int r = A.size();
-    while (l < r) {
-      final int m = (l + r) / 2;
-      if (A.get(m).getKey() >= target)
-        r = m;
-      else
-        l = m + 1;
-    }
-    return l;
-  }
+  def __iter__(self):
+    yield self.queryIndex
+    yield self.minX
+    yield self.minY
 
-  private MyPair[] getPairs(int[] nums1, int[] nums2) {
-    MyPair[] pairs = new MyPair[nums1.length];
-    for (int i = 0; i < nums1.length; ++i)
-      pairs[i] = new MyPair(nums1[i], nums2[i]);
-    Arrays.sort(pairs, (a, b) -> b.x - a.x);
-    return pairs;
-  }
 
-  private IndexedQuery[] getIndexedQueries(int[][] queries) {
-    IndexedQuery[] indexedQueries = new IndexedQuery[queries.length];
-    for (int i = 0; i < queries.length; ++i)
-      indexedQueries[i] = new IndexedQuery(i, queries[i][0], queries[i][1]);
-    Arrays.sort(indexedQueries, (a, b) -> b.minX - a.minX);
-    return indexedQueries;
-  }
-}
+class Solution:
+  def maximumSumQueries(self, nums1: List[int], nums2: List[int], queries: List[List[int]]) -> List[int]:
+    pairs = sorted([Pair(nums1[i], nums2[i])
+                   for i in range(len(nums1))], key=lambda p: p.x, reverse=True)
+    ans = [0] * len(queries)
+    stack = []  # [(y, x + y)]
+
+    pairsIndex = 0
+    for queryIndex, minX, minY in sorted([IndexedQuery(i, query[0], query[1])
+                                          for i, query in enumerate(queries)],
+                                         key=lambda iq: -iq.minX):
+      while pairsIndex < len(pairs) and pairs[pairsIndex].x >= minX:
+        # x + y is a better candidate. Given that x is decreasing, the
+        # condition "x + y >= stack[-1][1]" suggests that y is relatively
+        # larger, thereby making it a better candidate.
+        x, y = pairs[pairsIndex]
+        while stack and x + y >= stack[-1][1]:
+          stack.pop()
+        if not stack or y > stack[-1][0]:
+          stack.append((y, x + y))
+        pairsIndex += 1
+      j = self._firstGreaterEqual(stack, minY)
+      ans[queryIndex] = -1 if j == len(stack) else stack[j][1]
+
+    return ans
+
+  def _firstGreaterEqual(self, A: List[Tuple[int, int]], target: int) -> int:
+    l = 0
+    r = len(A)
+    while l < r:
+      m = (l + r) // 2
+      if A[m][0] >= target:
+        r = m
+      else:
+        l = m + 1
+    return l
