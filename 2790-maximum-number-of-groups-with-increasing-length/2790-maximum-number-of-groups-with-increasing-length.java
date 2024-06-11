431327283538bd9544f66aa3b65b311581dbdc36
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-12 00:23:00 +0530

    Time: 114 ms (62.50%), Space: 55.9 MB (92.86%) - LeetHub

--- 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.java
+++ 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.java
@@ -1,18 +1,17 @@
 class Solution {
- public:
-  int maxIncreasingGroups(vector<int>& usageLimits) {
-    int ans = 1;  // the next target length
+  public int maxIncreasingGroups(List<Integer> usageLimits) {
+    int ans = 1; // the next target length
     long availableLimits = 0;
 
-    ranges::sort(usageLimits);
+    Collections.sort(usageLimits);
 
-    for (const int usageLimit : usageLimits) {
+    for (final int usageLimit : usageLimits) {
       availableLimits += usageLimit;
       // Can create groups 1, 2, ..., ans.
-      if (availableLimits >= ans * static_cast<long>(ans + 1) / 2)
+      if (availableLimits >= ans * (long) (ans + 1) / 2)
         ++ans;
     }
 
     return ans - 1;
   }
-};
+}
