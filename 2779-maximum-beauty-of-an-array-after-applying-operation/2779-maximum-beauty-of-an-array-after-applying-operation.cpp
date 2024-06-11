41f93aeb79cd600dfd1afa22e9310345e50b7314
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-12 00:25:44 +0530

    Time: 177 ms (38.91%), Space: 103.3 MB (99.58%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,16 +1,16 @@
 class Solution {
  public:
   int maximumBeauty(vector<int>& nums, int k) {
-    // l and r track the maximum window instead of the valid window.
-    int l = 0;
-    int r = 0;
+    int ans = 0;
 
     ranges::sort(nums);
 
-    for (r = 0; r < nums.size(); ++r)
-      if (nums[r] - nums[l] > 2 * k)
+    for (int l = 0, r = 0; r < nums.size(); ++r) {
+      while (nums[r] - nums[l] > 2 * k)
         ++l;
+      ans = max(ans, r - l + 1);
+    }
 
-    return r - l;
+    return ans;
   }
 };
