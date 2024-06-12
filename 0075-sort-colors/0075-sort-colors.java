57cb046a25307ac965ec792b0d1c75f84ff60fcf
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-12 09:27:29 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (94.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,22 +1,19 @@
 class Solution {
   public void sortColors(int[] nums) {
-    int l = 0;               // The next 0 should be placed in l.
-    int r = nums.length - 1; // THe next 2 should be placed in r.
+    int zero = -1;
+    int one = -1;
+    int two = -1;
 
-    for (int i = 0; i <= r;)
-      if (nums[i] == 0)
-        swap(nums, i++, l++);
-      else if (nums[i] == 1)
-        ++i;
-      else
-        // We may swap a 0 to index i, but we're still not sure whether this 0
-        // is placed in the correct index, so we can't move pointer i.
-        swap(nums, i, r--);
-  }
-
-  private void swap(int[] nums, int i, int j) {
-    final int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
+    for (final int num : nums)
+      if (num == 0) {
+        nums[++two] = 2;
+        nums[++one] = 1;
+        nums[++zero] = 0;
+      } else if (num == 1) {
+        nums[++two] = 2;
+        nums[++one] = 1;
+      } else {
+        nums[++two] = 2;
+      }
   }
 }
