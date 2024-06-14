cf4717c8e5bd32bad9685acf6abcc79e74e5241b
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-15 00:24:18 +0530

    Time: 604 ms (81.41%), Space: 31.3 MB (17.95%) - LeetHub

--- 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.py
+++ 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.py
@@ -1,22 +1,19 @@
-class Solution {
-  public boolean checkArray(int[] nums, int k) {
-    if (k == 1)
-      return true;
+class Solution:
+  def checkArray(self, nums: List[int], k: int) -> bool:
+    if k == 1:
+      return True
 
-    int needDecrease = 0;
-    // Store nums[i - k + 1..i] with decreasing nums[i - k + 1].
-    Deque<Integer> dq = new ArrayDeque<>();
+    needDecrease = 0
+    # Store nums[i - k + 1..i] with decreasing nums[i - k + 1].
+    dq = collections.deque()
 
-    for (int i = 0; i < nums.length; ++i) {
-      if (i >= k)
-        needDecrease -= dq.pollFirst();
-      if (nums[i] < needDecrease)
-        return false;
-      final int decreasedNum = nums[i] - needDecrease;
-      dq.offerLast(decreasedNum);
-      needDecrease += decreasedNum;
-    }
+    for i, num in enumerate(nums):
+      if i >= k:
+        needDecrease -= dq.popleft()
+      if nums[i] < needDecrease:
+        return False
+      decreasedNum = nums[i] - needDecrease
+      dq.append(decreasedNum)
+      needDecrease += decreasedNum
 
-    return dq.getLast() == 0;
-  }
-}
+    return dq[-1] == 0
