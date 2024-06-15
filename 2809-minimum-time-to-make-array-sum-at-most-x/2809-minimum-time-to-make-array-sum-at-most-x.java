354314d2976a4d95e50886d22306bb5b7ec4890b
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-16 00:00:29 +0530

    Time: 27 ms (56.00%), Space: 48.7 MB (60.00%) - LeetHub

--- 2809-minimum-time-to-make-array-sum-at-most-x/2809-minimum-time-to-make-array-sum-at-most-x.java
+++ 2809-minimum-time-to-make-array-sum-at-most-x/2809-minimum-time-to-make-array-sum-at-most-x.java
@@ -3,9 +3,9 @@ class Solution {
     final int n = nums1.size();
     final int sum1 = nums1.stream().mapToInt(Integer::intValue).sum();
     final int sum2 = nums2.stream().mapToInt(Integer::intValue).sum();
-    // dp[j] := the maximum reduced value if we do j operations on the numbers
-    // so far
-    int[] dp = new int[n + 1];
+    // dp[i][j] := the maximum reduced value if we do j operations on the first
+    // i numbers
+    int[][] dp = new int[n + 1][n + 1];
     List<Pair<Integer, Integer>> sortedNums = new ArrayList<>();
 
     for (int i = 0; i < n; ++i)
@@ -16,19 +16,17 @@ class Solution {
     for (int i = 1; i <= n; ++i) {
       final int num2 = sortedNums.get(i - 1).getKey();
       final int num1 = sortedNums.get(i - 1).getValue();
-      for (int j = i; j > 0; --j)
-        dp[j] = Math.max(
-            // the maximum reduced value if we do j operations on the first
-            // i - 1 numbers
-            dp[j],
-            // the maximum reduced value if we do j - 1 operations on the first
-            // i - 1 numbers + making the i-th number of `nums1` to 0 at the
-            // j-th operation
-            dp[j - 1] + num2 * j + num1);
+      for (int j = 1; j <= i; ++j)
+        dp[i][j] = Math.max(
+            // max reduced value if we do j ops on the first i - 1 nums
+            dp[i - 1][j],
+            // max reduced value if we do j - 1 ops on the first i - 1 nums +
+            // making i-th num of nums1 to 0 at j-th operation
+            dp[i - 1][j - 1] + num2 * j + num1);
     }
 
     for (int op = 0; op <= n; ++op)
-      if (sum1 + sum2 * op - dp[op] <= x)
+      if (sum1 + sum2 * op - dp[n][op] <= x)
         return op;
 
     return -1;
