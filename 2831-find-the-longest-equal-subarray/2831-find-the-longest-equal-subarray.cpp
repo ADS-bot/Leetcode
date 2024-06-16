d0c3e95baa9ac60307cd075f46bba1e065d350f7
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-17 00:37:56 +0530

    Time: 260 ms (90.51%), Space: 183.2 MB (84.91%) - LeetHub

--- 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.cpp
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.cpp
@@ -4,9 +4,10 @@ class Solution {
     int ans = 0;
     unordered_map<int, int> count;
 
+    // l and r track the maximum window instead of the valid window.
     for (int l = 0, r = 0; r < nums.size(); ++r) {
       ans = max(ans, ++count[nums[r]]);
-      while (r - l + 1 - k > ans)
+      if (r - l + 1 - k > ans)
         --count[nums[l++]];
     }
 
