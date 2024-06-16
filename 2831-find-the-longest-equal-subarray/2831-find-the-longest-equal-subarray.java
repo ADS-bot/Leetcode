08890faee3c059a032b0c89deef64d5315a18c15
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-17 00:37:37 +0530

    Time: 67 ms (87.28%), Space: 62.4 MB (66.47%) - LeetHub

--- 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.java
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.java
@@ -3,10 +3,9 @@ class Solution {
     int ans = 0;
     Map<Integer, Integer> count = new HashMap<>();
 
-    // l and r track the maximum window instead of the valid window.
     for (int l = 0, r = 0; r < nums.size(); ++r) {
       ans = Math.max(ans, count.merge(nums.get(r), 1, Integer::sum));
-      if (r - l + 1 - k > ans)
+      while (r - l + 1 - k > ans)
         count.merge(nums.get(l++), -1, Integer::sum);
     }
 
