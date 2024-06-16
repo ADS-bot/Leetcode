ed3a95f41a887155cc00844b8296413d9f4bd1ab
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-16 13:28:40 +0530

    Time: 61 ms (31.12%), Space: 16.6 MB (69.93%) - LeetHub

--- 0330-patching-array/0330-patching-array.py
+++ 0330-patching-array/0330-patching-array.py
@@ -1,20 +1,17 @@
-class Solution {
- public:
-  int minPatches(vector<int>& nums, int n) {
-    int ans = 0;
-    int i = 0;      // nums' index
-    long miss = 1;  // the minimum sum in [1, n] we might miss
+class Solution:
+  def minPatches(self, nums: List[int], n: int) -> int:
+    ans = 0
+    i = 0  # nums' index
+    miss = 1  # the minimum sum in [1, n] we might miss
 
-    while (miss <= n)
-      if (i < nums.size() && nums[i] <= miss) {
-        miss += nums[i++];
-      } else {
-        // Greedily add `miss` itself to increase the range from
-        // [1, miss) to [1, 2 * miss).
-        miss += miss;
-        ++ans;
-      }
+    while miss <= n:
+      if i < len(nums) and nums[i] <= miss:
+        miss += nums[i]
+        i += 1
+      else:
+        # Greedily add `miss` itself to increase the range from
+        # [1, miss) to [1, 2 * miss).
+        miss += miss
+        ans += 1
 
-    return ans;
-  }
-};
+    return ans
