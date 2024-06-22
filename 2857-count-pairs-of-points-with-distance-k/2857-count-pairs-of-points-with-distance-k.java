c9e1e7ec600cd313641978b5ed8f6002b476be97
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-23 00:00:14 +0530

    Time: 1352 ms (7.32%), Space: 77.5 MB (24.39%) - LeetHub

--- 2857-count-pairs-of-points-with-distance-k/2857-count-pairs-of-points-with-distance-k.java
+++ 2857-count-pairs-of-points-with-distance-k/2857-count-pairs-of-points-with-distance-k.java
@@ -1,12 +1,18 @@
-class Solution:
-  def countPairs(self, coordinates: List[List[int]], k: int) -> int:
-    ans = 0
+class Solution {
+  public int countPairs(List<List<Integer>> coordinates, int k) {
+    int ans = 0;
 
-    for x in range(k + 1):
-      y = k - x
-      count = collections.Counter()
-      for xi, yi in coordinates:
-        ans += count[(xi ^ x, yi ^ y)]
-        count[(xi, yi)] += 1
+    for (int x = 0; x <= k; ++x) {
+      final int y = k - x;
+      Map<Pair<Integer, Integer>, Integer> count = new HashMap<>();
+      for (List<Integer> point : coordinates) {
+        final int xi = point.get(0);
+        final int yi = point.get(1);
+        ans += count.getOrDefault(new Pair<>(xi ^ x, yi ^ y), 0);
+        count.merge(new Pair<>(xi, yi), 1, Integer::sum);
+      }
+    }
 
-    return ans
+    return ans;
+  }
+}
