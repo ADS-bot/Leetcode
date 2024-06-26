759e44a2c10d3c75f8f15ce5351eda884d96de92
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-27 01:41:43 +0530

    Time: 31 ms (63.24%), Space: 62.9 MB (47.06%) - LeetHub

--- 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.java
+++ 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.java
@@ -1,21 +1,26 @@
-class Solution:
-  def numberOfGoodPartitions(self, nums: List[int]) -> int:
-    kMod = 1_000_000_007
-    ans = 1
-    # lastSeen[num] := the index of the last time `num` appeared
-    lastSeen = {}
+class Solution {
+  public int numberOfGoodPartitions(int[] nums) {
+    final int kMod = 1_000_000_007;
+    int ans = 1;
 
-    for i, num in enumerate(nums):
-      lastSeen[num] = i
+    // lastSeen[num] := the index of the last time `num` appeared
+    HashMap<Integer, Integer> lastSeen = new HashMap<>();
 
-    # Track the maximum right index of each running partition by ensuring that
-    # the first and last occurrences of a number fall within the same partition.
-    maxRight = 0
-    for i, num in enumerate(nums):
-      if i > maxRight:
-        # Start a new partition that starts from nums[i].
-        # Each partition doubles the total number of good partitions.
-        ans = ans * 2 % kMod
-      maxRight = max(maxRight, lastSeen[num])
+    for (int i = 0; i < nums.length; ++i)
+      lastSeen.put(nums[i], i);
 
-    return ans
+    // Track the maximum right index of each running partition by ensuring that
+    // the first and last occurrences of a number fall within the same
+    // partition.
+    int maxRight = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      if (i > maxRight)
+        // Start a new partition that starts from nums[i].
+        // Each partition doubles the total number of good partitions.
+        ans = (int) ((ans * 2L) % kMod);
+      maxRight = Math.max(maxRight, lastSeen.get(nums[i]));
+    }
+
+    return ans;
+  }
+}
