751f6ad8768205aadb735fff42842573f888976f
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-27 23:53:43 +0530

    Time: 19 ms (63.61%), Space: 24 MB (39.66%) - LeetHub

--- 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.cpp
+++ 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.cpp
@@ -2,13 +2,20 @@ class Solution {
  public:
   int minimumCoins(vector<int>& prices) {
     const int n = prices.size();
-    vector<int> dp(n + 1, INT_MAX);
-    dp[n] = 0;
+    int ans = 0;
+    using P = pair<int, int>;
+    // Stores (dp[i], i), where dp[i] := the minimum number of coins to acquire
+    // fruits[i:] (0-indexed).
+    priority_queue<P, vector<P>, greater<>> minHeap;
+    minHeap.emplace(0, n);
 
-    for (int i = n - 1; i >= 0; --i)
-      for (int j = i + 1; j <= min((i + 1) * 2, n); ++j)
-        dp[i] = min(dp[i], prices[i] + dp[j]);
+    for (int i = n - 1; i >= 0; --i) {
+      while (!minHeap.empty() && minHeap.top().second > (i + 1) * 2)
+        minHeap.pop();
+      ans = prices[i] + minHeap.top().first;
+      minHeap.emplace(ans, i);
+    }
 
-    return dp[0];
+    return ans;
   }
 };
