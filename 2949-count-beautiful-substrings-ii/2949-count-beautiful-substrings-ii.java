9f529d805bed10d8a0072aceed8a5f8300e1101c
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-27 23:57:36 +0530

    Time: 85 ms (71.43%), Space: 48.4 MB (42.86%) - LeetHub

--- 2949-count-beautiful-substrings-ii/2949-count-beautiful-substrings-ii.java
+++ 2949-count-beautiful-substrings-ii/2949-count-beautiful-substrings-ii.java
@@ -1,44 +1,37 @@
 class Solution {
- public:
   // Same as 2947. Count Beautiful Substrings I
-  int beautifulSubstrings(string s, int k) {
-    const int root = getRoot(k);
+  public int beautifulSubstrings(String s, int k) {
+    final int root = getRoot(k);
     int ans = 0;
     int vowels = 0;
     int vowelsMinusConsonants = 0;
     // {(vowels, vowelsMinusConsonants): count}
-    unordered_map<pair<int, int>, int, PairHash> prefixCount{{{0, 0}, 1}};
+    Map<Pair<Integer, Integer>, Integer> prefixCount = new HashMap<>();
+    prefixCount.put(new Pair<>(0, 0), 1);
 
-    for (const char c : s) {
+    for (final char c : s.toCharArray()) {
       if (isVowel(c)) {
         vowels = (vowels + 1) % root;
         ++vowelsMinusConsonants;
       } else {
         --vowelsMinusConsonants;
       }
-      const pair<int, int> prefix{vowels, vowelsMinusConsonants};
-      ans += prefixCount[prefix]++;
+      Pair<Integer, Integer> prefix = new Pair<>(vowels, vowelsMinusConsonants);
+      ans += prefixCount.getOrDefault(prefix, 0);
+      prefixCount.merge(prefix, 1, Integer::sum);
     }
 
     return ans;
   }
 
- private:
-  bool isVowel(char c) {
-    static constexpr string_view kVowels = "aeiouAEIOU";
-    return kVowels.find(c) != string_view::npos;
+  private boolean isVowel(char c) {
+    return "aeiou".indexOf(c) != -1;
   }
 
-  int getRoot(int k) {
+  private int getRoot(int k) {
     for (int i = 1; i <= k; ++i)
       if (i * i % k == 0)
         return i;
-    throw;
+    throw new IllegalArgumentException();
   }
-
-  struct PairHash {
-    size_t operator()(const pair<int, int>& p) const {
-      return p.first ^ p.second;
-    }
-  };
-};
+}
