3d99a66db99e85651c1c7409f946c73d50422d85
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-06-30 12:29:12 +0530

    Time: 177 ms (75.93%), Space: 137.4 MB (61.57%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -1,22 +1,23 @@
 class Solution {
-  public int[][] constructProductMatrix(int[][] grid) {
-    final int kMod = 12345;
-    final int m = grid.length;
-    final int n = grid[0].length;
-    int[][] ans = new int[m][n];
-    List<Integer> prefix = new ArrayList<>(Arrays.asList(1));
+ public:
+  vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+    constexpr int kMod = 12345;
+    const int m = grid.size();
+    const int n = grid[0].size();
+    vector<vector<int>> ans(m, vector<int>(n));
+    vector<int> prefix{1};
     int suffix = 1;
 
-    for (int[] row : grid)
-      for (int cell : row)
-        prefix.add((int) ((long) prefix.get(prefix.size() - 1) * cell % kMod));
+    for (const vector<int>& row : grid)
+      for (const int cell : row)
+        prefix.push_back(static_cast<long>(prefix.back()) * cell % kMod);
 
-    for (int i = m - 1; i >= 0; i--)
-      for (int j = n - 1; j >= 0; j--) {
-        ans[i][j] = (int) ((long) prefix.get(i * n + j) * suffix % kMod);
-        suffix = (int) ((long) suffix * grid[i][j] % kMod);
+    for (int i = m - 1; i >= 0; --i)
+      for (int j = n - 1; j >= 0; --j) {
+        ans[i][j] = prefix[i * n + j] * suffix % kMod;
+        suffix = static_cast<long>(suffix) * grid[i][j] % kMod;
       }
 
     return ans;
   }
-}
+};
