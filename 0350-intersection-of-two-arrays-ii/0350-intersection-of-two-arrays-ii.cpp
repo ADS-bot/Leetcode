3153dfee88839a8edec22d3d1b88d74ad03ee7c4
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-02 13:09:50 +0530

    Time: 7 ms (27.72%), Space: 12.5 MB (75.54%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,24 +1,37 @@
 class Solution {
  public:
   vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    if (nums1.size() > nums2.size())
+      return intersect(nums2, nums1);
+
+    vector<int> ans;
+    int lowerBound = 0;  // the lower bound for the binary search
+
     ranges::sort(nums1);
     ranges::sort(nums2);
 
-    vector<int> ans;
-    int i = 0;  // nums1's index
-    int j = 0;  // nums2's index
-
-    while (i < nums1.size() && j < nums2.size())
-      if (nums1[i] < nums2[j]) {
-        ++i;
-      } else if (nums1[i] > nums2[j]) {
-        ++j;
-      } else {
-        ans.push_back(nums1[i]);
-        ++i;
-        ++j;
+    for (const int num : nums1) {
+      const int i = binarySearch(nums2, lowerBound, num);
+      if (i < nums2.size() && nums2[i] == num) {
+        ans.push_back(num);
+        lowerBound = i + 1;
       }
+    }
 
     return ans;
+  }
+
+ private:
+  int binarySearch(vector<int>& nums2, int lo, int target) {
+    int l = lo;
+    int r = nums2.size();
+    while (l < r) {
+      const int m = (l + r) / 2;
+      if (nums2[m] >= target)
+        r = m;
+      else
+        l = m + 1;
+    }
+    return l;
   }
 };
