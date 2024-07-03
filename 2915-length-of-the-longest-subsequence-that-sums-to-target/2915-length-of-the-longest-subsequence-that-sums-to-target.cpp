4820eaa6a025040d4ddae1c40a878a610d8d1df7
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-03 23:50:18 +0530

    Time: 92 ms (91.34%), Space: 31.6 MB (97.27%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,25 +1,15 @@
 class Solution {
  public:
   int lengthOfLongestSubsequence(vector<int>& nums, int target) {
-    const int n = nums.size();
-    // dp[i][j] := the maximum length of any subsequence of the first i numbers
-    // that sum to j
-    vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
+    // dp[i] := the maximum length of any subsequence of numbers so far that
+    // sum to j
+    vector<int> dp(target + 1);
 
-    for (int i = 0; i <= n; ++i)
-      dp[i][0] = 0;
+    for (const int num : nums)
+      for (int i = target; i >= num; --i)
+        if (i == num || dp[i - num] > 0)
+          dp[i] = max(dp[i], 1 + dp[i - num]);
 
-    for (int i = 1; i <= n; ++i) {
-      const int num = nums[i - 1];
-      for (int j = 1; j <= target; ++j)
-        // Case 1: Skip `num`.
-        if (j < num || dp[i - 1][j - num] == -1)
-          dp[i][j] = dp[i - 1][j];
-        // Case 2: Skip `num` or pick `num`.
-        else
-          dp[i][j] = max(dp[i - 1][j], 1 + dp[i - 1][j - num]);
-    }
-
-    return dp[n][target];
+    return dp[target] > 0 ? dp[target] : -1;
   }
 };
