a1ca9207de97506406ecb83c6e11391a3d5f71b6
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-04 23:03:33 +0530

    Time: 475 ms (45.19%), Space: 156.2 MB (88.94%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -1,88 +1,58 @@
-class Multiset {
-  public void add(int num) {
-    map.merge(num, 1, Integer::sum);
-    ++sz;
-  }
-
-  public void remove(int num) {
-    map.merge(num, -1, Integer::sum);
-    if (map.get(num) == 0)
-      map.remove(num);
-    --sz;
-  }
-
-  public int min() {
-    return map.firstEntry().getKey();
-  }
-
-  public int max() {
-    return map.lastEntry().getKey();
-  }
-
-  public int size() {
-    return sz;
-  }
-
-  public boolean contains(int num) {
-    return map.containsKey(num);
-  }
-
-  private TreeMap<Integer, Integer> map = new TreeMap<>();
-  private int sz = 0;
-}
-
 class Solution {
-  public long minimumCost(int[] nums, int k, int dist) {
+ public:
+  long long minimumCost(vector<int>& nums, int k, int dist) {
     // Equivalently, the problem is to find nums[0] + the minimum sum of the top
     // k - 1 numbers in nums[i..i + dist], where i > 0 and i + dist < n.
     long windowSum = 0;
-    Multiset selected = new Multiset();
-    Multiset candidates = new Multiset();
+    multiset<int> selected;
+    multiset<int> candidates;
 
     for (int i = 1; i <= dist + 1; ++i) {
       windowSum += nums[i];
-      selected.add(nums[i]);
+      selected.insert(nums[i]);
     }
 
     windowSum = balance(windowSum, selected, candidates, k);
     long minWindowSum = windowSum;
 
-    for (int i = dist + 2; i < nums.length; ++i) {
-      final int outOfScope = nums[i - dist - 1];
-      if (selected.contains(outOfScope)) {
+    for (int i = dist + 2; i < nums.size(); ++i) {
+      const int outOfScope = nums[i - dist - 1];
+      if (selected.find(outOfScope) != selected.end()) {
         windowSum -= outOfScope;
-        selected.remove(outOfScope);
+        selected.erase(selected.find(outOfScope));
       } else {
-        candidates.remove(outOfScope);
+        candidates.erase(candidates.find(outOfScope));
       }
-      if (nums[i] < selected.max()) { // nums[i] is a better number.
+      if (nums[i] < *selected.rbegin()) {  // nums[i] is a better number.
         windowSum += nums[i];
-        selected.add(nums[i]);
+        selected.insert(nums[i]);
       } else {
-        candidates.add(nums[i]);
+        candidates.insert(nums[i]);
       }
       windowSum = balance(windowSum, selected, candidates, k);
-      minWindowSum = Math.min(minWindowSum, windowSum);
+      minWindowSum = min(minWindowSum, windowSum);
     }
 
     return nums[0] + minWindowSum;
   }
 
+ private:
   // Returns the updated `windowSum` by balancing the multiset `selected` to
   // keep the top k - 1 numbers.
-  private long balance(long windowSum, Multiset selected, Multiset candidates, int k) {
+  long balance(long windowSum, multiset<int>& selected,
+               multiset<int>& candidates, int k) {
     while (selected.size() < k - 1) {
-      final int minCandidate = candidates.min();
+      const int minCandidate = *candidates.begin();
       windowSum += minCandidate;
-      selected.add(minCandidate);
-      candidates.remove(minCandidate);
+      selected.insert(minCandidate);
+      candidates.erase(candidates.find(minCandidate));
     }
     while (selected.size() > k - 1) {
-      final int maxSelected = selected.max();
+      const int maxSelected = *selected.rbegin();
       windowSum -= maxSelected;
-      selected.remove(maxSelected);
-      candidates.add(maxSelected);
+      selected.erase(selected.find(maxSelected));
+      candidates.insert(maxSelected);
     }
     return windowSum;
   }
-}
+};
