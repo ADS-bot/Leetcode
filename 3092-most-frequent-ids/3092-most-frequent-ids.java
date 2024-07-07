b16d73fbc6606604a5ebaea669d12aca125ba90f
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-08 00:42:24 +0530

    Time: 144 ms (51.35%), Space: 64.1 MB (90.81%) - LeetHub

--- 3092-most-frequent-ids/3092-most-frequent-ids.java
+++ 3092-most-frequent-ids/3092-most-frequent-ids.java
@@ -1,24 +1,27 @@
-from sortedcontainers import SortedDict
+class Solution {
+  public long[] mostFrequentIDs(int[] nums, int[] freq) {
+    long[] ans = new long[nums.length];
+    Map<Integer, Long> numCount = new HashMap<>();      // {num: freq}
+    TreeMap<Long, Integer> freqCount = new TreeMap<>(); // {num's freq: freq}
 
+    for (int i = 0; i < nums.length; ++i) {
+      final int num = nums[i];
+      final int f = freq[i];
+      if (numCount.containsKey(num)) {
+        final long numFreq = numCount.get(num);
+        if (freqCount.merge(numFreq, -1, Integer::sum) == 0)
+          freqCount.remove(numFreq);
+      }
+      final long newFreq = numCount.getOrDefault(num, 0L) + f;
+      if (newFreq == 0) {
+        numCount.remove(num);
+      } else {
+        numCount.put(num, newFreq);
+        freqCount.merge(newFreq, 1, Integer::sum);
+      }
+      ans[i] = freqCount.isEmpty() ? 0 : freqCount.lastKey();
+    }
 
-class Solution:
-  def mostFrequentIDs(self, nums: List[int], freq: List[int]) -> List[int]:
-    ans = []
-    numCount = collections.Counter()  # {num: freq}
-    freqCount = SortedDict()  # {num's freq: freq}
-
-    for num, f in zip(nums, freq):
-      if numCount[num] > 0:
-        numFreq = numCount[num]
-        freqCount[numFreq] -= 1
-        if freqCount[numFreq] == 0:
-          del freqCount[numFreq]
-      newFreq = numCount[num] + f
-      if newFreq == 0:
-        del numCount[num]
-      else:
-        numCount[num] = newFreq
-        freqCount[newFreq] = freqCount.get(newFreq, 0) + 1
-      ans.append(freqCount.peekitem(-1)[0] if freqCount else 0)
-
-    return ans
+    return ans;
+  }
+}
