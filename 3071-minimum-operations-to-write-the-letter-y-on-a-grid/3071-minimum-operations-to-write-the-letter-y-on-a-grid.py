3ce84bc548739d24ea3b7a4b8eb081b22bc8078b
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-07 12:15:08 +0530

    Time: 446 ms (14.84%), Space: 16.5 MB (90.80%) - LeetHub

--- 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.py
+++ 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.py
@@ -1,25 +1,22 @@
-class Solution {
-  public int minimumOperationsToWriteY(int[][] grid) {
-    return Math.min(Math.min(Math.min(getOperations(grid, 0, 1), getOperations(grid, 0, 2)),
-                             Math.min(getOperations(grid, 1, 0), getOperations(grid, 1, 2))),
-                    Math.min(getOperations(grid, 2, 0), getOperations(grid, 2, 1)));
-  }
+class Solution:
+  def minimumOperationsToWriteY(self, grid: List[List[int]]) -> int:
+    n = len(grid)
+    mid = n // 2
 
-  // Returns the number of operations to turn Y into a and non-Y into b.
-  private int getOperations(int[][] grid, int a, int b) {
-    final int n = grid.length;
-    final int mid = n / 2;
-    int operations = 0;
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < n; j++)
-        // For the 'Y' pattern, before the midpoint, check the diagonal and
-        // anti-diagonal. After the midpoint, check the middle column.
-        if ((i < mid && (i == j || i + j == n - 1)) || (i >= mid && j == mid)) {
-          if (grid[i][j] != a)
-            ++operations;
-        } else if (grid[i][j] != b) {
-          ++operations;
-        }
-    return operations;
-  }
-}
+    def getOperations(a: int, b: int) -> int:
+      """Returns the number of operations to turn Y into a and non-Y into b."""
+      operations = 0
+      for i, row in enumerate(grid):
+        for j, num in enumerate(row):
+          # For the 'Y' pattern, before the midpoint, check the diagonal and
+          # anti-diagonal. After the midpoint, check the middle column.
+          if (i < mid and (i == j or i + j == n - 1)) or i >= mid and j == mid:
+            if num != a:
+              operations += 1
+          elif num != b:
+            operations += 1
+      return operations
+
+    return min(getOperations(0, 1), getOperations(0, 2),
+               getOperations(1, 0), getOperations(1, 2),
+               getOperations(2, 0), getOperations(2, 1))
