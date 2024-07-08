0ee2368ad3499b73f0b73d4d06dfb0ef7825a8d7
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-08 11:26:59 +0530

    Time: 556 ms (100.00%), Space: 30 MB (100.00%) - LeetHub

--- 3041-maximize-consecutive-elements-in-an-array-after-modification/3041-maximize-consecutive-elements-in-an-array-after-modification.py
+++ 3041-maximize-consecutive-elements-in-an-array-after-modification/3041-maximize-consecutive-elements-in-an-array-after-modification.py
@@ -1,12 +1,27 @@
 class Solution:
   def maxSelectedElements(self, nums: List[int]) -> int:
-    ans = 0
-    # {num: the length of the longest consecutive elements ending in num}
-    dp = {}
+    ans = 1
+    prev = -math.inf
+    # the length of the longest consecutive elements (seq0) ending in the
+    # previous number
+    dp0 = 1
+    # the length of the longest consecutive elements (seq1) ending in the
+    # previous number + 1
+    dp1 = 1
 
     for num in sorted(nums):
-      dp[num + 1] = dp.get(num, 0) + 1
-      dp[num] = dp.get(num - 1, 0) + 1
-      ans = max(ans, dp[num], dp[num + 1])
+      if num == prev:
+        dp1 = dp0 + 1  # Append `num + 1` to seq0.
+      elif num == prev + 1:
+        dp0 += 1  # Append `num` to seq0.
+        dp1 += 1  # Add 1 to every number in seq0 and append `num + 1` to seq0.
+      elif num == prev + 2:
+        dp0 = dp1 + 1  # Append `num` to seq1.
+        dp1 = 1        # Start a new sequence [`num + 1`].
+      else:
+        dp0 = 1  # Start a new sequence [`num`].
+        dp1 = 1  # Start a new sequence [`num + 1`].
+      ans = max(ans, dp0, dp1)
+      prev = num
 
     return ans
