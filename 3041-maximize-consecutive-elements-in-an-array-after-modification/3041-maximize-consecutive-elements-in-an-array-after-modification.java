b747f5804d9ffacad9fcfdb334ea4a7817914b65
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-08 11:26:05 +0530

    Time: 30 ms (98.72%), Space: 57 MB (89.74%) - LeetHub

--- 3041-maximize-consecutive-elements-in-an-array-after-modification/3041-maximize-consecutive-elements-in-an-array-after-modification.java
+++ 3041-maximize-consecutive-elements-in-an-array-after-modification/3041-maximize-consecutive-elements-in-an-array-after-modification.java
@@ -1,15 +1,31 @@
 class Solution {
   public int maxSelectedElements(int[] nums) {
-    int ans = 0;
-    // {num: the length of the longest consecutive elements ending in num}
-    HashMap<Integer, Integer> dp = new HashMap<>();
+    int ans = 1;
+    int prev = Integer.MIN_VALUE;
+    // the length of the longest consecutive elements (seq0) ending in the
+    // previous number
+    int dp0 = 1;
+    // the length of the longest consecutive elements (seq1) ending in the
+    // previous number + 1
+    int dp1 = 1;
 
     Arrays.sort(nums);
 
     for (final int num : nums) {
-      dp.put(num + 1, dp.getOrDefault(num, 0) + 1);
-      dp.put(num, dp.getOrDefault(num - 1, 0) + 1);
-      ans = Math.max(ans, Math.max(dp.get(num), dp.get(num + 1)));
+      if (num == prev) {
+        dp1 = dp0 + 1; // Append `num + 1` to seq0.
+      } else if (num == prev + 1) {
+        ++dp0; // Append `num` to seq0.
+        ++dp1; // Add 1 to every number in seq0 and append `num + 1` to seq0.
+      } else if (num == prev + 2) {
+        dp0 = dp1 + 1; // Append `num` to seq1.
+        dp1 = 1;       // Start a new sequence [`num + 1`].
+      } else {
+        dp0 = 1; // Start a new sequence [`num`].
+        dp1 = 1; // Start a new sequence [`num + 1`].
+      }
+      ans = Math.max(ans, Math.max(dp0, dp1));
+      prev = num;
     }
 
     return ans;
