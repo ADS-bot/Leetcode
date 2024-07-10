188daceacaec1b373966eb45f839ac4af2c902fc
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-10 19:21:06 +0530

    Time: 4 ms (80.00%), Space: 43.6 MB (85.00%) - LeetHub

--- 3082-find-the-sum-of-the-power-of-all-subsequences/3082-find-the-sum-of-the-power-of-all-subsequences.java
+++ 3082-find-the-sum-of-the-power-of-all-subsequences/3082-find-the-sum-of-the-power-of-all-subsequences.java
@@ -1,18 +1,33 @@
-class Solution:
-  def sumOfPower(self, nums: List[int], k: int) -> int:
-    kMod = 1_000_000_007
+class Solution {
+  public int sumOfPower(int[] nums, int k) {
+    Integer[][] mem = new Integer[nums.length][k + 1];
+    return sumOfPower(nums, 0, k, mem);
+  }
 
-    @functools.lru_cache(None)
-    def dp(i: int, j: int) -> int:
-      """Returns the number of subsequences in nums[i..n) that sums to j."""
-      if j == 0:
-        # For each of the remaining number, we can either pick it or skip it.
-        return pow(2, len(nums) - i, kMod)
-      if i == len(nums) or j < 0:
-        return 0
-        # Case 1: Include nums[i] in the subsequence and pick it.
-        # Case 2: Include nums[i] in the subsequence and skip it.
-        # Case 3: Exclude nums[i] in the subsequence.
-      return (dp(i + 1, j - nums[i]) + 2 * dp(i + 1, j)) % kMod
+  private static final int kMod = 1_000_000_007;
 
-    return dp(0, k)
+  // Returns the number of subsequences in nums[i..n) that sums to j.
+  private int sumOfPower(int[] nums, int i, int j, Integer[][] mem) {
+    if (j == 0)
+      // For each of the remaining number, we can either pick it or skip it.
+      return (int) modPow(2, nums.length - i);
+    if (i == nums.length || j < 0)
+      return 0;
+    if (mem[i][j] != null)
+      return mem[i][j];
+    // Case 1: Include nums[i] in the subsequence and pick it.
+    // Case 2: Include nums[i] in the subsequence and skip it.
+    // Case 3: Exclude nums[i] in the subsequence.
+    return mem[i][j] = (int) ((sumOfPower(nums, i + 1, j - nums[i], mem) + //
+                               2L * sumOfPower(nums, i + 1, j, mem)) %
+                              kMod);
+  }
+
+  private long modPow(long x, long n) {
+    if (n == 0)
+      return 1;
+    if (n % 2 == 1)
+      return x * modPow(x % kMod, (n - 1)) % kMod;
+    return modPow(x * x % kMod, (n / 2)) % kMod;
+  }
+}
