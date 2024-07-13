576c219c6fb46523fe381b1d59bb002bfb90a8e4
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-13 22:59:47 +0530

    Time: 21 ms (84.12%), Space: 153.8 MB (27.18%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,18 +1,21 @@
-class Solution:
-  def numberOfSubmatrices(self, grid: List[List[str]]) -> int:
-    m = len(grid)
-    n = len(grid[0])
-    ans = 0
-    # x[i][j] := the number of 'X' in grid[0..i)[0..j)
-    x = [[0] * (n + 1) for _ in range(m + 1)]
-    # y[i][j] := the number of 'Y' in grid[0..i)[0..j)
-    y = [[0] * (n + 1) for _ in range(m + 1)]
+class Solution {
+  public int numberOfSubmatrices(char[][] grid) {
+    final int m = grid.length;
+    final int n = grid[0].length;
+    int ans = 0;
+    // x[i][j] := the number of 'X' in grid[0..i)[0..j)
+    int[][] x = new int[m + 1][n + 1];
+    // y[i][j] := the number of 'Y' in grid[0..i)[0..j)
+    int[][] y = new int[m + 1][n + 1];
 
-    for i, row in enumerate(grid):
-      for j, cell in enumerate(row):
-        x[i + 1][j + 1] = (cell == 'X') + x[i][j + 1] + x[i + 1][j] - x[i][j]
-        y[i + 1][j + 1] = (cell == 'Y') + y[i][j + 1] + y[i + 1][j] - y[i][j]
-        if x[i + 1][j + 1] > 0 and x[i + 1][j + 1] == y[i + 1][j + 1]:
-          ans += 1
+    for (int i = 0; i < m; ++i)
+      for (int j = 0; j < n; ++j) {
+        x[i + 1][j + 1] = (grid[i][j] == 'X' ? 1 : 0) + x[i][j + 1] + x[i + 1][j] - x[i][j];
+        y[i + 1][j + 1] = (grid[i][j] == 'Y' ? 1 : 0) + y[i][j + 1] + y[i + 1][j] - y[i][j];
+        if (x[i + 1][j + 1] > 0 && x[i + 1][j + 1] == y[i + 1][j + 1])
+          ++ans;
+      }
 
-    return ans
+    return ans;
+  }
+}
