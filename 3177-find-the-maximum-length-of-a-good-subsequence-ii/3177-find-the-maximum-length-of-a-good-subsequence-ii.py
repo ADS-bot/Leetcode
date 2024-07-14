fbef5077c92617a83d3b3a97e118ca0823553228
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-14 19:49:51 +0530

    Time: 1766 ms (35.51%), Space: 24.8 MB (50.50%) - LeetHub

--- 3177-find-the-maximum-length-of-a-good-subsequence-ii/3177-find-the-maximum-length-of-a-good-subsequence-ii.py
+++ 3177-find-the-maximum-length-of-a-good-subsequence-ii/3177-find-the-maximum-length-of-a-good-subsequence-ii.py
@@ -1,23 +1,11 @@
-class Solution {
- public:
-  // Same as 3176. Find the Maximum Length of a Good Subsequence I
-  int maximumLength(vector<int>& nums, int k) {
-    // dp[count][num] := the maximum length of a good subsequence with at most
-    // `count` indices where seq[i] != seq[i + 1] and it ends in `num`.
-    vector<unordered_map<int, int>> dp(k + 1);
-    // maxLen[count] := the maximum length of a good subsequence with `count`
-    // indices where seq[i] != seq[i + 1]
-    vector<int> maxLen(k + 1);
-
-    for (const int num : nums)
-      for (int count = k; count >= 0; --count) {
-        // Append `num` to the subsequence.
-        ++dp[count][num];
-        if (count > 0)
-          dp[count][num] = max(dp[count][num], maxLen[count - 1] + 1);
-        maxLen[count] = max(maxLen[count], dp[count][num]);
-      }
-
-    return maxLen[k];
-  }
-};
+class Solution:
+  def maximumLength(self, nums: List[int], k: int) -> int:
+    dp = [collections.Counter() for _ in range(k + 1)]
+    maxLen = [0] * (k + 1)
+    for num in nums:
+      for count in range(k, -1, -1):
+        dp[count][num] += 1
+        if count > 0:
+          dp[count][num] = max(dp[count][num], maxLen[count - 1] + 1)
+        maxLen[count] = max(maxLen[count], dp[count][num])
+    return maxLen[k]
