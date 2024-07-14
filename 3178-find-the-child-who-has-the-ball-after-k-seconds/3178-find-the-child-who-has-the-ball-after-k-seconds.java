887324c260df6e9642c4a07206d68fa3daff8dde
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-14 20:18:32 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (51.98%) - LeetHub

--- 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.java
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.java
@@ -1,9 +1,8 @@
 class Solution {
- public:
-  int numberOfChild(int n, int k) {
+  public int numberOfChild(int n, int k) {
     // the time for the ball to return to 0
-    const int roundTime = 2 * (n - 1);
-    const int pos = k % roundTime;
+    final int roundTime = 2 * (n - 1);
+    final int pos = k % roundTime;
     return pos < n ? pos : roundTime - pos;
   }
-};
+}
