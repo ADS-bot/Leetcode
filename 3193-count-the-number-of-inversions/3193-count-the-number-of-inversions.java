0761f819510936a8b9df146e962ab9fd7141a77b
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-18 00:39:50 +0530

    Time: 816 ms (5.07%), Space: 48.4 MB (20.85%) - LeetHub

--- 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.java
+++ 3193-count-the-number-of-inversions/3193-count-the-number-of-inversions.java
@@ -1,17 +1,67 @@
-class Solution:
-    def numberOfPermutations(self, n: int, requirements: List[List[int]]) -> int:
-        requirements.sort()
-        dp = [0] * (requirements[-1][-1] + 1)
-        dp[0] = 1
-        r_map = {}
-        for r in requirements:
-            r_map[r[0]] = r[1]
-        for i in range(1, n):
-            for j in range(len(dp) - 1, -1, -1):
-                if i - 1 not in r_map:
-                    dp[j] = sum(dp[max(0, j - i):j + 1])
-                elif 0 <= j - r_map[i - 1] <= min(i, j):
-                    dp[j] = dp[r_map[i - 1]]
-                else:
-                    dp[j] = 0
-        return dp[-1] % 1000000007
+class Solution {
+
+    static int MOD = (int) 1e9 + 7;
+
+    int dp[][];
+    boolean vis[][];
+
+    long solve(int n, HashMap<Integer, Integer> req, int index, int count) {
+
+        if (index == 0) {
+            if (count == 0) {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        if (req.containsKey(index)) {
+            if (count != req.get(index)) {
+                return 0;
+            }
+        }
+
+        if (vis[index][count]) {
+            return dp[index][count];
+        }
+
+        long ans = 0;
+
+        for (int i = 0; i <= index; i++) {
+            int contribution = i;
+
+            if (contribution > count) {
+                continue;
+            }
+
+            ans = ans + solve(n, req, index - 1, count - contribution);
+            ans = ans % MOD;
+        }
+
+        vis[index][count] = true;
+        dp[index][count] = (int) ans;
+
+        return ans;
+    }
+
+    public int numberOfPermutations(int n, int[][] requirements) {
+
+        HashMap<Integer, Integer> reqMap = new HashMap<>();
+
+        int maxCount = 0;
+
+        for (int i = 0; i < requirements.length; i++) {
+            reqMap.put(requirements[i][0], requirements[i][1]);
+            maxCount = Math.max(maxCount, requirements[i][1]);
+        }
+
+        if(reqMap.containsKey(0) && reqMap.get(0) > 0){
+            return 0;
+        }
+
+        dp = new int[n + 1][maxCount + 1];
+        vis = new boolean[n + 1][maxCount + 1];
+
+        return (int) solve(n, reqMap, n - 1, reqMap.get(n - 1));
+    }
+}
