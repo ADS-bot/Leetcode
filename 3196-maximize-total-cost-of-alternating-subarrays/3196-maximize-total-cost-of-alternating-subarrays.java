f5b2850e35c3af4b05e2a44d601a5874cda53cf5
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-07-19 09:15:37 +0530

    Time: 1 ms (100.00%), Space: 61.6 MB (65.67%) - LeetHub

--- 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.java
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.java
@@ -1,11 +1,13 @@
 class Solution {
   public long maximumTotalCost(int[] nums) {
-    long keep = nums[0]; // the maximum cost if the last number is kept
-    long flip = nums[0]; // the maximum cost if the last number is flipped
+    // A small trick so that we don't need to handle the edge case and can use
+    // ranged-based for loop.
+    long keep = Long.MIN_VALUE / 2; // the maximum cost if the last number is kept
+    long flip = 0;                  // the maximum cost if the last number is flipped
 
-    for (int i = 1; i < nums.length; ++i) {
-      final long keepCurr = Math.max(keep, flip) + nums[i];
-      final long flipCurr = keep - nums[i];
+    for (final int num : nums) {
+      final long keepCurr = Math.max(keep, flip) + num;
+      final long flipCurr = keep - num;
       keep = keepCurr;
       flip = flipCurr;
     }
