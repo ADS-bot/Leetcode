908523786cde46f781a5013e510c2337fa3ce358
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-08-10 09:39:01 +0530

    Time: 104 ms (69.56%), Space: 16.9 MB (69.96%) - LeetHub

--- 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.py
+++ 0959-regions-cut-by-slashes/0959-regions-cut-by-slashes.py
@@ -2,16 +2,13 @@ class UnionFind:
     def __init__(self, size):
         self.parent = list(range(size))
         self.rank = [1] * size
-    
     def find(self, x):
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
-    
     def union(self, x, y):
         rootX = self.find(x)
         rootY = self.find(y)
-        
         if rootX != rootY:
             if self.rank[rootX] > self.rank[rootY]:
                 self.parent[rootY] = rootX
@@ -20,37 +17,26 @@ class UnionFind:
             else:
                 self.parent[rootY] = rootX
                 self.rank[rootX] += 1
-
 class Solution:
     def regionsBySlashes(self, grid: List[str]) -> int:
         n = len(grid)
-        uf = UnionFind(4 * n * n)  # 4 triangles per 1x1 square
-        
+        uf = UnionFind(4 * n * n)
         for i in range(n):
             for j in range(n):
                 index = 4 * (i * n + j)
-                
                 if grid[i][j] == '/':
-                    # Connect triangles 0-3, 1-2
                     uf.union(index + 0, index + 3)
                     uf.union(index + 1, index + 2)
                 elif grid[i][j] == '\\':
-                    # Connect triangles 0-1, 2-3
                     uf.union(index + 0, index + 1)
                     uf.union(index + 2, index + 3)
                 else:
-                    # Connect all triangles in the square
                     uf.union(index + 0, index + 1)
                     uf.union(index + 1, index + 2)
                     uf.union(index + 2, index + 3)
-                
-                # Union with right and bottom neighbors
-                if j + 1 < n:  # Connect right neighbor
+                if j + 1 < n:
                     uf.union(index + 1, 4 * (i * n + (j + 1)) + 3)
-                if i + 1 < n:  # Connect bottom neighbor
+                if i + 1 < n:
                     uf.union(index + 2, 4 * ((i + 1) * n + j) + 0)
-        
-        # Count number of connected components
         region_count = sum(1 for i in range(4 * n * n) if uf.find(i) == i)
-        
         return region_count
