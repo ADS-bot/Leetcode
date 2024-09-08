a015e62b81eb4e85cccbf2d1c2d6c0c8119b6436
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-09-08 10:56:21 +0530

    Time: 241 ms (100.00%), Space: 109.6 MB (100.00%) - LeetHub

---
+++ 3281-maximize-score-of-numbers-in-ranges/3281-maximize-score-of-numbers-in-ranges.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxPossibleScore(vector<int>& start, int d) {
+        sort(start.begin(), start.end());
+
+        auto check = [&](long long lim) {
+            long long now = start[0];
+            for (int i = 1; i < start.size(); i++) {
+                now = max(now + lim, 1LL * start[i]);
+                if (now > start[i] + d) return false;
+            }
+            return true;
+        };
+
+        long long head = 0, tail = 2e9;
+        while (head < tail) {
+            long long mid = (head + tail + 1) >> 1;
+            if (check(mid)) head = mid;
+            else tail = mid - 1;
+        }
+        return head;
+    }
+};
