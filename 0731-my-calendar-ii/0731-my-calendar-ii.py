fdfc7a0cd05672b331bf386d36f26bfcd3b3b0b7
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-09-27 10:19:20 +0530

    Time: 247 ms (83.12%), Space: 17.4 MB (66.75%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -0,0 +1,15 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.non_overlapping =[]        
+        self.overlapping =[]
+    def book(self, start: int, end: int) -> bool:
+        for s,e in self.overlapping:
+            if end > s and e > start:
+                return False
+            
+        for s,e in self.non_overlapping:
+            if end > s and e > start:
+                self.overlapping.append((max(s,start),min(e,end)))
+        self.non_overlapping.append((start,end))
+        return True
