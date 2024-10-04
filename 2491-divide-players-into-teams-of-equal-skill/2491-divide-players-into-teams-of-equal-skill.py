fe4a411d71171efe10951fda1ee599c4845a589c
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-10-04 10:51:12 +0530

    Time: 376 ms (95.75%), Space: 30.4 MB (13.81%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -4,11 +4,9 @@ class Solution:
     teamSkill = sum(skill) // (n // 2)
     ans = 0
     count = collections.Counter(skill)
-
     for s, freq in count.items():
       requiredSkill = teamSkill - s
       if count[requiredSkill] != freq:
         return -1
       ans += s * requiredSkill * freq
-
     return ans // 2
