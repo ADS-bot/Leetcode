f2ffcae67d2cf3dcab5224fd01d2c75ded94b171
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-10-08 22:58:59 +0530

    Time: 158 ms (26.57%), Space: 17 MB (52.78%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.py
+++ 0087-scramble-string/0087-scramble-string.py
@@ -1,42 +1,30 @@
 class Solution:
     def isScramble(self, s1: str, s2: str) -> bool:
-        # If the lengths don't match, it's not possible
         if len(s1) != len(s2):
             return False
         
-        # Memoization dictionary to store already computed results
-        memo = {}
+        n = len(s1)
 
-        def dfs(s1, s2):
-            # If already computed, return the cached result
-            if (s1, s2) in memo:
-                return memo[(s1, s2)]
+        # dp[k][i][j] is True if s1[i:i+k] is a scrambled string of s2[j:j+k]
+        dp = [[[False] * n for _ in range(n)] for _ in range(n + 1)]
 
-            # Base cases
-            if s1 == s2:
-                memo[(s1, s2)] = True
-                return True
-            
-            # If character counts don't match, return False
-            if sorted(s1) != sorted(s2):  # This can be replaced by a character count check for further optimization
-                memo[(s1, s2)] = False
-                return False
+        # Initialize the base case for substrings of length 1
+        for i in range(n):
+            for j in range(n):
+                dp[1][i][j] = (s1[i] == s2[j])
 
-            n = len(s1)
+        # Build up the solution for substrings of length 2 to n
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                for j in range(n - length + 1):
+                    for k in range(1, length):
+                        # Case 1: No swap
+                        if dp[k][i][j] and dp[length - k][i + k][j + k]:
+                            dp[length][i][j] = True
+                            break
+                        # Case 2: With swap
+                        if dp[k][i][j + length - k] and dp[length - k][i + k][j]:
+                            dp[length][i][j] = True
+                            break
 
-            # Try to split at every possible index and check recursively
-            for i in range(1, n):
-                # Without swapping
-                if dfs(s1[:i], s2[:i]) and dfs(s1[i:], s2[i:]):
-                    memo[(s1, s2)] = True
-                    return True
-                # With swapping
-                if dfs(s1[:i], s2[-i:]) and dfs(s1[i:], s2[:-i]):
-                    memo[(s1, s2)] = True
-                    return True
-
-            # If no valid scramble is found
-            memo[(s1, s2)] = False
-            return False
-
-        return dfs(s1, s2)
+        return dp[n][0][0]
