9f06bb794d4d030fc3c41a42dbc50bbed243cfee
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-10-14 10:52:33 +0530

    Time: 729 ms (56.64%), Space: 31.1 MB (78.48%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.py
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.py
@@ -3,10 +3,8 @@ class Solution:
     ans = 0
     maxHeap = [-num for num in nums]
     heapq.heapify(maxHeap)
-
     for _ in range(k):
       num = -heapq.heappop(maxHeap)
       ans += num
       heapq.heappush(maxHeap, -math.ceil(num / 3))
-
     return ans
