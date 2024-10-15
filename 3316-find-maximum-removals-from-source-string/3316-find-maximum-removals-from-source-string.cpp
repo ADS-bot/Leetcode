b6960e7a8541d984d14898544436983a052e8ded
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-10-15 13:42:32 +0530

    Time: 240 ms (38.70%), Space: 94 MB (20.84%) - LeetHub

--- 3316-find-maximum-removals-from-source-string/3316-find-maximum-removals-from-source-string.cpp
+++ 3316-find-maximum-removals-from-source-string/3316-find-maximum-removals-from-source-string.cpp
@@ -1,30 +1,23 @@
 class Solution {
  public:
   int maxRemovals(string source, string pattern, vector<int>& targetIndices) {
+    const int m = source.length();
+    const int n = pattern.length();
     const unordered_set<int> target{targetIndices.begin(), targetIndices.end()};
-    vector<vector<int>> mem(source.length(), vector<int>(pattern.length(), -1));
-    const int ans = maxRemovals(source, pattern, 0, 0, target, mem);
-    return ans == INT_MIN ? 0 : ans;
-  }
+    // dp[i][j] := the maximum number of operations that can be performed for
+    // source[i..m) and pattern[j..n)
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MIN));
+    dp[m][n] = 0;
 
- private:
-  // Returns the maximum number of operations that can be performed for
-  // source[i..m) and pattern[j..n).
-  int maxRemovals(const string& source, const string& pattern, int i, int j,
-                  const unordered_set<int>& target, vector<vector<int>>& mem) {
-    if (i == source.length())
-      return j == pattern.length() ? 0 : INT_MIN;
-    if (j == pattern.length())
-      return (target.contains(i) ? 1 : 0) +
-             maxRemovals(source, pattern, i + 1, j, target, mem);
-    if (mem[i][j] != -1)
-      return mem[i][j];
-    const int pick =
-        source[i] == pattern[j]
-            ? maxRemovals(source, pattern, i + 1, j + 1, target, mem)
-            : INT_MIN;
-    const int skip = (target.contains(i) ? 1 : 0) +
-                     maxRemovals(source, pattern, i + 1, j, target, mem);
-    return mem[i][j] = max(pick, skip);
-  };
+    for (int i = m - 1; i >= 0; --i) {
+      dp[i][n] = (target.contains(i) ? 1 : 0) + dp[i + 1][n];
+      for (int j = n - 1; j >= 0; --j) {
+        const int pick = source[i] == pattern[j] ? dp[i + 1][j + 1] : INT_MIN;
+        const int skip = (target.contains(i) ? 1 : 0) + dp[i + 1][j];
+        dp[i][j] = max(pick, skip);
+      }
+    }
+
+    return dp[0][0] == INT_MIN ? 0 : dp[0][0];
+  }
 };
