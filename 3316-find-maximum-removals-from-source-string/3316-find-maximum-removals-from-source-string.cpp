a16c098b46700cebb3393848b5b1494631e7611e
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-10-15 13:42:43 +0530

    Time: 213 ms (41.68%), Space: 93.2 MB (28.82%) - LeetHub

--- 3316-find-maximum-removals-from-source-string/3316-find-maximum-removals-from-source-string.cpp
+++ 3316-find-maximum-removals-from-source-string/3316-find-maximum-removals-from-source-string.cpp
@@ -4,20 +4,22 @@ class Solution {
     const int m = source.length();
     const int n = pattern.length();
     const unordered_set<int> target{targetIndices.begin(), targetIndices.end()};
-    // dp[i][j] := the maximum number of operations that can be performed for
-    // source[i..m) and pattern[j..n)
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MIN));
-    dp[m][n] = 0;
+    // dp[j] := the maximum number of operations that can be performed for
+    // source so far and pattern[j..n)
+    vector<int> dp(n + 1, INT_MIN);
+    dp[n] = 0;
 
     for (int i = m - 1; i >= 0; --i) {
-      dp[i][n] = (target.contains(i) ? 1 : 0) + dp[i + 1][n];
+      vector<int> newDp(dp);
+      newDp[n] = (target.contains(i) ? 1 : 0) + dp[n];
       for (int j = n - 1; j >= 0; --j) {
-        const int pick = source[i] == pattern[j] ? dp[i + 1][j + 1] : INT_MIN;
-        const int skip = (target.contains(i) ? 1 : 0) + dp[i + 1][j];
-        dp[i][j] = max(pick, skip);
+        const int pick = source[i] == pattern[j] ? dp[j + 1] : INT_MIN;
+        const int skip = (target.contains(i) ? 1 : 0) + dp[j];
+        newDp[j] = max(pick, skip);
       }
+      dp = std::move(newDp);
     }
 
-    return dp[0][0] == INT_MIN ? 0 : dp[0][0];
+    return dp[0] == INT_MIN ? 0 : dp[0];
   }
 };
