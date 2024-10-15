4abb7dbd510b4e1066a0dfeac2eba89a8c0fed96
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-10-15 13:41:52 +0530

    Time: 1 ms (100.00%), Space: 44.9 MB (62.98%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.java
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.java
@@ -1,23 +1,21 @@
 class Solution {
- public:
   // Same as 3314. Construct the Minimum Bitwise Array I
-  vector<int> minBitwiseArray(vector<int>& nums) {
-    vector<int> ans;
+  public int[] minBitwiseArray(List<Integer> nums) {
+    int[] ans = new int[nums.size()];
 
-    for (const int num : nums)
-      ans.push_back(num == 2 ? -1 : num - getLeadingOneOfLastGroupOfOnes(num));
+    for (int i = 0; i < nums.size(); ++i)
+      ans[i] = nums.get(i) == 2 ? -1 : nums.get(i) - getLeadingOneOfLastGroupOfOnes(nums.get(i));
 
     return ans;
   }
 
- private:
   // Returns the leading one of the last group of 1s in the binary
   // representation of num. For example, if num = 0b10111, the leading one of
   // the last group of 1s is 0b100.
-  int getLeadingOneOfLastGroupOfOnes(int num) {
+  private int getLeadingOneOfLastGroupOfOnes(int num) {
     int leadingOne = 1;
     while ((num & leadingOne) > 0)
       leadingOne <<= 1;
     return leadingOne >> 1;
   }
-};
+}
