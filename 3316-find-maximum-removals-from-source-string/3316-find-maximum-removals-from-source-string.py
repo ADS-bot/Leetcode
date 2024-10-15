3f54ad297fe4cbd9f5a45fee34d559672f408e4e
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-10-15 13:43:46 +0530

    Time: 2571 ms (22.67%), Space: 17 MB (73.22%) - LeetHub

--- 3316-find-maximum-removals-from-source-string/3316-find-maximum-removals-from-source-string.py
+++ 3316-find-maximum-removals-from-source-string/3316-find-maximum-removals-from-source-string.py
@@ -8,16 +8,16 @@ class Solution:
     m = len(source)
     n = len(pattern)
     target = set(targetIndices)
-    # dp[i][j] := the maximum number of operations that can be performed for
-    # source[i..m) and pattern[j..n)
-    dp = [[-math.inf] * (n + 1) for _ in range(m + 1)]
-    dp[m][n] = 0
+    dp = [-math.inf] * (n + 1)
+    dp[n] = 0
 
     for i in reversed(range(m)):
-      dp[i][n] = int(i in target) + dp[i + 1][n]
-      for j in reversed(range(n)):
-        pick = dp[i + 1][j + 1] if source[i] == pattern[j] else -math.inf
-        skip = int(i in target) + dp[i + 1][j]
-        dp[i][j] = max(pick, skip)
+      newDp = dp[:]
+      newDp[n] = int(i in target) + dp[n]
+      for j in range(n):
+        pick = dp[j + 1] if source[i] == pattern[j] else -math.inf
+        skip = int(i in target) + dp[j]
+        newDp[j] = max(pick, skip)
+      dp = newDp
 
-    return 0 if dp[0][0] == -math.inf else dp[0][0]
+    return 0 if dp[0] == -math.inf else dp[0]
