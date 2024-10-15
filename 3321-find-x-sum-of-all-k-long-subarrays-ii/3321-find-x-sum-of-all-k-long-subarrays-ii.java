282426e40ac7938e7e354f8cf5d47759109b20ae
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-10-15 13:48:21 +0530

    Time: 731 ms (53.73%), Space: 69.3 MB (70.15%) - LeetHub

--- 3321-find-x-sum-of-all-k-long-subarrays-ii/3321-find-x-sum-of-all-k-long-subarrays-ii.java
+++ 3321-find-x-sum-of-all-k-long-subarrays-ii/3321-find-x-sum-of-all-k-long-subarrays-ii.java
@@ -1,55 +1,60 @@
 class Solution {
- public:
   // Same as 3318. Find X-Sum of All K-Long Subarrays I
-  vector<long long> findXSum(vector<int>& nums, int k, int x) {
-    vector<long long> ans;
-    long windowSum = 0;
-    unordered_map<int, int> count;
-    multiset<pair<int, int>> top;  // the top x elements
-    multiset<pair<int, int>> bot;  // the rest of the elements
+  public long[] findXSum(int[] nums, int k, int x) {
+    long[] ans = new long[nums.length - k + 1];
+    Map<Integer, Integer> count = new HashMap<>();
+    TreeSet<Pair<Integer, Integer>> top =
+        new TreeSet<>((a, b)
+                          -> a.getKey().equals(b.getKey()) ? a.getValue().compareTo(b.getValue())
+                                                           : a.getKey().compareTo(b.getKey()));
+    TreeSet<Pair<Integer, Integer>> bot =
+        new TreeSet<>((a, b)
+                          -> a.getKey().equals(b.getKey()) ? a.getValue().compareTo(b.getValue())
+                                                           : a.getKey().compareTo(b.getKey()));
 
-    // Updates the count of num by freq and the window sum accordingly.
-    auto update = [&count, &top, &bot, &windowSum](int num, int freq) -> void {
-      if (count[num] > 0) {  // Clean up the old count.
-        if (auto it = bot.find({count[num], num}); it != bot.end()) {
-          bot.erase(it);
-        } else {
-          it = top.find({count[num], num});
-          top.erase(it);
-          windowSum -= static_cast<long>(num) * count[num];
-        }
-      }
-      count[num] += freq;
-      if (count[num] > 0)
-        bot.insert({count[num], num});
-    };
-
-    for (int i = 0; i < nums.size(); ++i) {
-      update(nums[i], 1);
+    for (int i = 0; i < nums.length; ++i) {
+      update(nums[i], 1, count, top, bot);
       if (i >= k)
-        update(nums[i - k], -1);
+        update(nums[i - k], -1, count, top, bot);
       // Move the bottom elements to the top if needed.
-      while (!bot.empty() && top.size() < x) {
-        const auto [countB, b] = *bot.rbegin();
-        bot.erase(--bot.end());
-        top.insert({countB, b});
-        windowSum += static_cast<long>(b) * countB;
+      while (!bot.isEmpty() && top.size() < x) {
+        Pair<Integer, Integer> pair = bot.pollLast();
+        top.add(pair);
+        windowSum += (long) pair.getValue() * pair.getKey();
       }
       // Swap the bottom and top elements if needed.
-      while (!bot.empty() && *bot.rbegin() > *top.begin()) {
-        const auto [countB, b] = *bot.rbegin();
-        const auto [countT, t] = *top.begin();
-        bot.erase(--bot.end());
-        top.erase(top.begin());
-        bot.insert({countT, t});
-        top.insert({countB, b});
-        windowSum += static_cast<long>(b) * countB;
-        windowSum -= static_cast<long>(t) * countT;
+      while (!bot.isEmpty() && (bot.last().getKey() > top.first().getKey() ||
+                                bot.last().getKey().equals(top.first().getKey()) &&
+                                    bot.last().getValue() > top.first().getValue())) {
+        Pair<Integer, Integer> pairB = bot.pollLast();
+        Pair<Integer, Integer> pairT = top.pollFirst();
+        top.add(pairB);
+        bot.add(pairT);
+        windowSum += (long) pairB.getValue() * pairB.getKey();
+        windowSum -= (long) pairT.getValue() * pairT.getKey();
       }
       if (i >= k - 1)
-        ans.push_back(windowSum);
+        ans[i - k + 1] = windowSum;
     }
-
     return ans;
   }
-};
+
+  private long windowSum = 0;
+
+  // Updates the count of num by freq and the window sum accordingly.
+  private void update(int num, int freq, Map<Integer, Integer> count,
+                      TreeSet<Pair<Integer, Integer>> top, TreeSet<Pair<Integer, Integer>> bot) {
+    if (count.getOrDefault(num, 0) > 0) { // Clean up the old count.
+      Pair<Integer, Integer> pair = new Pair<>(count.get(num), num);
+      if (bot.remove(pair)) {
+        // Do nothing.
+      } else {
+        top.remove(pair);
+        windowSum -= (long) num * count.get(num);
+      }
+    }
+    count.merge(num, freq, Integer::sum);
+    if (count.get(num) > 0)
+      bot.add(new Pair<>(count.get(num), num));
+  }
+}
