e51354c516fc96ac03556ffa6928d198100abe87
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-10-22 14:57:00 +0530

    Time: 82 ms (97.96%), Space: 59.3 MB (7.52%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.py
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.py
@@ -1,7 +1,6 @@
 class Solution:
   def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
     levelSums = []
-
     def dfs(root: Optional[TreeNode], level: int) -> None:
       if not root:
         return
@@ -10,9 +9,7 @@ class Solution:
       levelSums[level] += root.val
       dfs(root.left, level + 1)
       dfs(root.right, level + 1)
-
     dfs(root, 0)
     if len(levelSums) < k:
       return -1
-
     return sorted(levelSums, reverse=True)[k - 1]
