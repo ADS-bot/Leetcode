cd543f4a931cfc8daed7b764a3464c43f6ac4547
Arghadeep <116936743+ADS-bot@users.noreply.github.com>
2024-10-31 10:31:49 +0530

    Time: 2047 ms (44.00%), Space: 447.9 MB (5.44%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -2,13 +2,8 @@ class Solution:
   def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
     robot.sort()
     factory.sort()
-
     @functools.lru_cache(None)
     def dp(i: int, j: int, k: int) -> int:
-      """
-      Returns the minimum distance to fix robot[i..n) with factory[j..n), where
-      factory[j] already fixed k robots.
-      """
       if i == len(robot):
         return 0
       if j == len(factory):
@@ -18,5 +13,4 @@ class Solution:
       useFactory = dp(i + 1, j, k + 1) + abs(robot[i] - position) \
           if limit > k else math.inf
       return min(skipFactory, useFactory)
-
     return dp(0, 0, 0)
